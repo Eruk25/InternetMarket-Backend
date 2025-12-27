dead55a364e70a1b5f8263a8fb1a7db3efba7f2c
Danila <danilamatush72@gmail.com>
2025-12-27 21:40:20 +0300

    feat(application): implement GetUserByIdQuery in GetUserByIdQueryHandler

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Get/GetUserByIdQueryHandler.cs
@@ -0,0 +1,34 @@
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.Repositories;
+using MediatR;
+using InternetMarket.UserService.Domain;
+
+namespace InternetMarket.UserService.Application.Users.Get
+{
+    public class GetUserByIdQueryHandler : IRequestHandler<GetUserByIdQuery, UserDto>
+    {
+        private readonly IUserRepository _userRepository;
+
+        public GetUserByIdQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<UserDto> Handle(GetUserByIdQuery request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+                throw new ArgumentNullException();
+
+            var user = await _userRepository.GetByIdAsync(request.Id);
+
+            if (user is null)
+                throw new KeyNotFoundException($"User with id {request.Id} was not founded");
+
+            return new UserDto(user.Id, user.Name, user.Email, user.Role);
+        }
+    }
+}
