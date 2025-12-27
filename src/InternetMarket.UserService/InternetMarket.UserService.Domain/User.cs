405b843454c3ea1e6522c7b8df52fd5e735c2edb
Danila <danilamatush72@gmail.com>
2025-12-28 02:22:30 +0300

    feat(domain): add exceptions to updating methods

--- src/InternetMarket.UserService/InternetMarket.UserService.Domain/User.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Domain/User.cs
@@ -24,25 +24,29 @@ public class User
 
     public void UpdateName(string name)
     {
-        if (!string.IsNullOrWhiteSpace(name))
-            Name = name;
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentNullException("Name cannot be null or empty", nameof(name));
+        Name = name;
     }
 
     public void UpdateEmail(string email)
     {
         if (!string.IsNullOrWhiteSpace(email))
-            Email = email;
+            throw new ArgumentNullException("Email cannot be null or empty", nameof(email));
+        Email = email;
     }
 
     public void UpdatePassword(string password)
     {
         if (!string.IsNullOrWhiteSpace(password))
-            Password = password;
+            throw new ArgumentNullException("Password cannot be null or empty", nameof(password));
+        Password = password;
     }
 
     public void UpdateRole(string role)
     {
         if (!string.IsNullOrWhiteSpace(role))
-            Role = role;
+            throw new ArgumentNullException("Role cannot be null or empty", nameof(role));
+        Role = role;
     }
 }
