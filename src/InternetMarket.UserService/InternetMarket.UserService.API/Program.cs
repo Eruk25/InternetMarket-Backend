e87088e661328b1ebda9f87660e634d2f3a7a186
Danila <danilamatush72@gmail.com>
2025-12-28 23:18:21 +0300

    feat(presentation): register presentation, application, infrastructure in DI

--- src/InternetMarket.UserService/InternetMarket.UserService.API/Program.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/Program.cs
@@ -1,5 +1,13 @@
+using InternetMarket.UserService.API.Exstensions;
+using InternetMarket.UserService.Application.Extensions;
+using InternetMarket.UserService.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddPresentation(builder.Configuration)
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 var app = builder.Build();
 
 app.UseHttpsRedirection();
