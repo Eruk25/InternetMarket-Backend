d9d925b6a9351b2a620be72a260c42e030adb9f2
Danila <danilamatush72@gmail.com>
2025-12-28 22:01:06 +0300

    feat(infrastructure): implement ITokenGenerator in JwtGenerator

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Implementations/JWTGenerator/JwtGenerator.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.TokenGenerator;
+using InternetMarket.UserService.Domain;
+using Microsoft.Extensions.Options;
+using Microsoft.IdentityModel.Tokens;
+
+namespace InternetMarket.UserService.Infrastructure.Implementations.JWTGenerator
+{
+    public class JwtGenerator : ITokenGenerator
+    {
+        private readonly IOptions<AuthOptions> _options;
+
+        public JwtGenerator(IOptions<AuthOptions> options)
+        {
+            _options = options;
+        }
+
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                new Claim(ClaimTypes.Name, user.Name),
+                new Claim(ClaimTypes.Email, user.Email),
+                new Claim(ClaimTypes.Role, user.Role.ToString())
+            };
+
+            var token = new JwtSecurityToken(
+                issuer: _options.Value.Issuer,
+                audience: _options.Value.Audience,
+                claims: claims,
+                expires: DateTime.UtcNow.Add(_options.Value.ExpireTime),
+                signingCredentials:
+                new SigningCredentials(
+                    new SymmetricSecurityKey(
+                        System.Text.Encoding.UTF8.GetBytes(_options.Value.SecretKey)),
+                        SecurityAlgorithms.HmacSha256));
+            return new JwtSecurityTokenHandler().WriteToken(token);
+        }
+    }
+}
