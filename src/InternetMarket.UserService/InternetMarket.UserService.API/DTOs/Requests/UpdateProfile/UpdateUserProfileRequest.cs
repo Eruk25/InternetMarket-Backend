046fb19e60069083f938607b704101918442721f
Danila <danilamatush72@gmail.com>
2025-12-28 23:39:43 +0300

    feat(presentation): add UpdateUserProfileRequest

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/DTOs/Requests/UpdateProfile/UpdateUserProfileRequest.cs
@@ -0,0 +1,10 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MediatR;
+
+namespace InternetMarket.UserService.API.DTOs.Requests.UpdateProfile
+{
+    public record UpdateUserProfileRequest(string Name, string Email);
+}
