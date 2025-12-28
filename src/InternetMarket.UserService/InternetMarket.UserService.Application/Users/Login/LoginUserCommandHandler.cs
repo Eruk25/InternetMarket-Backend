956ecf70e417da26ac6be6e36d31eb9fe6998677
Danila <danilamatush72@gmail.com>
2025-12-28 22:03:53 +0300

    feat(application): implement LoginUserCommand in LoginUserCommandHandler

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Login/LoginUserCommandHandler.cs
@@ -0,0 +1,40 @@
+using System.Reflection.Metadata;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.Repositories;
+using MediatR;
+using System.Runtime.Serialization;
+using InternetMarket.UserService.Application.Abstractions.PasswordHasher;
+using InternetMarket.UserService.Application.Abstractions.TokenGenerator;
+
+namespace InternetMarket.UserService.Application.Users.Login
+{
+    public class LoginUserCommandHandler : IRequestHandler<LoginUserCommand, string>
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IPasswordHasher _passwordHasher;
+        private readonly ITokenGenerator _tokenGenerator;
+
+        public LoginUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, ITokenGenerator tokenGenerator)
+        {
+            _userRepository = userRepository;
+            _passwordHasher = passwordHasher;
+            _tokenGenerator = tokenGenerator;
+        }
+
+        public async Task<string> Handle(LoginUserCommand request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+
+            if (user is null || !_passwordHasher.VerifyPassword(request.Password, user.Password))
+                throw new InvalidOperationException("Invalid email or password.");
+
+            return _tokenGenerator.GenerateToken(user);
+        }
+    }
+}
