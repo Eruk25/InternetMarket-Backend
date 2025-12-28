047f334cd0d5e13a16bf0b2c7ff1808aaa57682c
Danila <danilamatush72@gmail.com>
2025-12-28 15:55:07 +0300

    feat(presentation): add Register method to AuthController

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/Controllers/AuthController.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.API.DTOs.Requests.Register;
+using InternetMarket.UserService.Application.Users.Register;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InternetMarket.UserService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AuthController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("register")]
+        public async Task<IActionResult> RegisterAsync(RegisterUserRequest request)
+        {
+            await _mediator.Send(new RegisterUserCommand(request.Name, request.Email, request.Password));
+            return Ok();
+        }
+
+    }
+}
