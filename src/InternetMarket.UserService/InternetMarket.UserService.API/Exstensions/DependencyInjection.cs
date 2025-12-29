25a75b2fd15c544711a65027e9a868fceea1d276
Danila <danilamatush72@gmail.com>
2025-12-29 10:52:46 +0300

    feat(presentation): register AuthenticationScheme and Authorization in DI

--- src/InternetMarket.UserService/InternetMarket.UserService.API/Exstensions/DependencyInjection.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/Exstensions/DependencyInjection.cs
@@ -1,7 +1,11 @@
+using System.Security.Claims;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace InternetMarket.UserService.API.Exstensions
 {
@@ -10,6 +14,25 @@ namespace InternetMarket.UserService.API.Exstensions
         public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
         {
             services.AddControllers();
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = configuration["AuthOptions:Issuer"],
+                        ValidAudience = configuration["AuthOptions:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(configuration["AuthOptions"]!)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+                });
+            services.AddAuthorization();
             return services;
         }
     }
