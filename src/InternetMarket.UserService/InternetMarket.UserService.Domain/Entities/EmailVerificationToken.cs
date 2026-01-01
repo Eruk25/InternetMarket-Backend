754defa6bb166b4bff45b76941af86f8f9559c40
Danila <danilamatush72@gmail.com>
2026-01-01 21:53:22 +0300

    refactor(domain): change User field to Upper case

--- src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/EmailVerificationToken.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/EmailVerificationToken.cs
@@ -9,7 +9,7 @@ namespace InternetMarket.UserService.Domain.Entities
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public User? user { get; set; }
+        public User? User { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ExpiresAt { get; set; }
     }
