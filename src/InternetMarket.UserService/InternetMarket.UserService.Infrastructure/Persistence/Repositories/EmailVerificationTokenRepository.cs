44691cc0318fec3e4a0a0de39dce037384884074
Danila <danilamatush72@gmail.com>
2026-01-01 22:08:30 +0300

    feat(infrastructure): implement IEmailVerificationTokenRepository in EmailVerificationTokenRepository

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Persistence/Repositories/EmailVerificationTokenRepository.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.Repositories;
+using InternetMarket.UserService.Domain.Entities;
+using InternetMarket.UserService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetMarket.UserService.Infrastructure.Persistence.Repositories
+{
+    public class EmailVerificationTokenRepository : IEmailVerificationTokenRepository
+    {
+        private readonly UserContext _context;
+
+        public EmailVerificationTokenRepository(UserContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<EmailVerificationToken?> GetByIdAsync(Guid Id)
+        {
+            var token = await _context.Tokens
+                .FirstOrDefaultAsync(t => t.Id == Id);
+            return token;
+        }
+
+        public async Task DeleteAsync(EmailVerificationToken token)
+        {
+            _context.Tokens.Remove(token);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task CreateAsync(EmailVerificationToken token)
+        {
+            await _context.Tokens.AddAsync(token);
+            await _context.SaveChangesAsync();
+        }
+    }
+}
