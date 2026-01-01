bf812813e0702c03dbb30df38fd150bdf5fb40ca
Danila <danilamatush72@gmail.com>
2026-01-01 22:22:00 +0300

    refactor(application): change returned value from EmailVerificationToken to EmailVerificationToken?

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IEmailVerificationTokenRepository.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IEmailVerificationTokenRepository.cs
@@ -7,7 +7,7 @@ namespace InternetMarket.UserService.Application.Abstractions.Repositories
 {
     public interface IEmailVerificationTokenRepository
     {
-        public Task<Domain.Entities.EmailVerificationToken> GetByIdAsync(Guid Id);
+        public Task<Domain.Entities.EmailVerificationToken?> GetByIdAsync(Guid Id);
         public Task CreateAsync(Domain.Entities.EmailVerificationToken token);
         public Task DeleteAsync(Domain.Entities.EmailVerificationToken token);
     }
