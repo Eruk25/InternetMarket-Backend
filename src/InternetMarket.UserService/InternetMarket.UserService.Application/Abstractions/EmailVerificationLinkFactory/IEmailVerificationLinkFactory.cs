f931f5542bb36299d0d0a7cf7a6b4348466c7b97
Danila <danilamatush72@gmail.com>
2026-01-01 21:54:55 +0300

    refactor(application): add specific using for EmailVerificationToken model

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/EmailVerificationLinkFactory/IEmailVerificationLinkFactory.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/EmailVerificationLinkFactory/IEmailVerificationLinkFactory.cs
@@ -1,13 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using InternetMarket.UserService.Domain.Entities;
-
 namespace InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory
 {
     public interface IEmailVerificationLinkFactory
     {
-        public string GenerateLink(EmailVerificationToken token);
+        public string GenerateLink(Domain.Entities.EmailVerificationToken token);
     }
 }
