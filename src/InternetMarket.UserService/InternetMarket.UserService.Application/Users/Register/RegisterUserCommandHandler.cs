ee739d40afa6c36519aaef7256d48809fad253e5
Danila <danilamatush72@gmail.com>
2026-01-01 23:02:11 +0300

    feat(application): add EmailVerification to RegisterUserCommandHandler

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Register/RegisterUserCommandHandler.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Register/RegisterUserCommandHandler.cs
@@ -2,9 +2,13 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.EmailSender;
+using InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory;
 using InternetMarket.UserService.Application.Abstractions.PasswordHasher;
 using InternetMarket.UserService.Application.Abstractions.Repositories;
+using InternetMarket.UserService.Application.DTOs.EmailMetadata;
 using InternetMarket.UserService.Domain;
+using InternetMarket.UserService.Domain.Entities;
 using MediatR;
 
 namespace InternetMarket.UserService.Application.Users.Register
@@ -12,12 +16,19 @@ namespace InternetMarket.UserService.Application.Users.Register
     public class RegisterUserCommandHandler : IRequestHandler<RegisterUserCommand>
     {
         private readonly IUserRepository _userRepository;
+        private readonly IEmailVerificationTokenRepository _emailVerificationTokenRepository;
+        private readonly IEmailVerificationLinkFactory _emailVerificationLinkFactory;
         private readonly IPasswordHasher _passwordHasher;
+        private readonly IEmailService _emailService;
 
-        public RegisterUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher)
+        public RegisterUserCommandHandler(IUserRepository userRepository, IPasswordHasher passwordHasher, IEmailService emailService,
+            IEmailVerificationTokenRepository emailVerificationTokenRepository, IEmailVerificationLinkFactory emailVerificationLinkFactory)
         {
             _userRepository = userRepository;
+            _emailVerificationTokenRepository = emailVerificationTokenRepository;
+            _emailVerificationLinkFactory = emailVerificationLinkFactory;
             _passwordHasher = passwordHasher;
+            _emailService = emailService;
         }
 
         public async Task Handle(RegisterUserCommand request, CancellationToken cancellationToken)
@@ -29,6 +40,18 @@ namespace InternetMarket.UserService.Application.Users.Register
             var user = new User(request.Name, request.Email, _passwordHasher.HashPassword(request.Password), guid);
 
             await _userRepository.CreateAsync(user);
+
+            var token = new Domain.Entities.EmailVerificationToken
+            {
+                Id = Guid.NewGuid(),
+                UserId = user.Id,
+                CreatedAt = DateTime.UtcNow,
+                ExpiresAt = DateTime.UtcNow.AddMinutes(30)
+            };
+            await _emailVerificationTokenRepository.CreateAsync(token);
+            string verificationLink = _emailVerificationLinkFactory.GenerateLink(token);
+            EmailMetadata emailMetadata = new(user.Email, "From InternetMarket", $"To verify email address <a hraf={verificationLink}>click here</a>");
+            await _emailService.SendAsync(emailMetadata);
         }
     }
 }
