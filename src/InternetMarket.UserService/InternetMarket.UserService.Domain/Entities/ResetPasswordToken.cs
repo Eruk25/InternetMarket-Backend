98e98c60030f970c1e6bd7b88b0735f5c6c8dec8
Danila <danilamatush72@gmail.com>
2026-01-02 18:37:28 +0300

    refactor(domain): change model name from ResetPaswwordToken to ResetPasswordToken

--- src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/ResetPasswordToken.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/ResetPasswordToken.cs
@@ -5,11 +5,11 @@ using System.Threading.Tasks;
 
 namespace InternetMarket.UserService.Domain.Entities
 {
-    public class ResetPaswwordToken
+    public class ResetPasswordToken
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
         public User User { get; set; }
-        public Guid ExpiresAt { get; set; }
+        public DateTime ExpiresAt { get; set; }
     }
 }
