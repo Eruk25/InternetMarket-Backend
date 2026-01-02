c143443de26599b99f201a7c5f4d59c02a88d31a
Danila <danilamatush72@gmail.com>
2026-01-02 19:08:44 +0300

    refactor(application): change returned type from ResetPasswordToken to ResetPasswordToken?

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IResetPasswordTokenRepository.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IResetPasswordTokenRepository.cs
@@ -7,7 +7,7 @@ namespace InternetMarket.UserService.Application.Abstractions.Repositories
 {
     public interface IResetPasswordTokenRepository
     {
-        public Task<Domain.Entities.ResetPasswordToken> GetByIdAsync(Guid Id);
+        public Task<Domain.Entities.ResetPasswordToken?> GetByIdAsync(Guid Id);
         public Task CreateAsync(Domain.Entities.ResetPasswordToken token);
         public Task DeleteAsync(Domain.Entities.ResetPasswordToken token);
     }
