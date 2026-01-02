e14bf42d49bb2d0649174cf7f567c75d68c972bb
Danila <danilamatush72@gmail.com>
2026-01-02 20:12:07 +0300

    feat(presentation): add FotgotPassword and ResetPassword methods

--- src/InternetMarket.UserService/InternetMarket.UserService.API/Controllers/AuthController.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/Controllers/AuthController.cs
@@ -5,8 +5,10 @@ using System.Threading.Tasks;
 using InternetMarket.UserService.API.DTOs.Requests.Login;
 using InternetMarket.UserService.API.DTOs.Requests.Register;
 using InternetMarket.UserService.Application.EmailVerificationToken.VerifyEmail;
+using InternetMarket.UserService.Application.ResetPasswordToken;
 using InternetMarket.UserService.Application.Users.Login;
 using InternetMarket.UserService.Application.Users.Register;
+using InternetMarket.UserService.Application.Users.Update.UpdateUserPassword;
 using MediatR;
 using Microsoft.AspNetCore.Identity.Data;
 using Microsoft.AspNetCore.Mvc;
@@ -45,5 +47,23 @@ namespace InternetMarket.UserService.API.Controllers
             return success ? Ok() : BadRequest();
         }
 
+        [HttpPost]
+        [Route("forgot-password")]
+        public async Task<ActionResult> ForgotPasswordAsync([FromBody] ForgotPasswordRequest request)
+        {
+            await _mediator.Send(new ForgotPasswordCommand(request.Email));
+            return Ok();
+        }
+
+        [HttpPatch]
+        [Route("reset-password")]
+        public async Task<ActionResult> ResetPasswordAsync([FromBody] ResetPasswordRequest request)
+        {
+            await _mediator.Send(new UpdateUserPasswordCommand(
+                request.Email,
+                request.ResetCode,
+                request.NewPassword));
+            return Ok();
+        }
     }
 }
