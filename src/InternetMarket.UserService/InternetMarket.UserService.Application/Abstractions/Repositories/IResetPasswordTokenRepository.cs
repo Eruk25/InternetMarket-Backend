59ef4b823860d1000c39ce070ba5b4e05df6e760
Danila <danilamatush72@gmail.com>
2026-01-02 18:57:23 +0300

    feat(application): add IResetPasswordTokenRepository

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IResetPasswordTokenRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.UserService.Application.Abstractions.Repositories
+{
+    public interface IResetPasswordTokenRepository
+    {
+        public Task<Domain.Entities.ResetPasswordToken> GetByIdAsync(Guid Id);
+        public Task CreateAsync(Domain.Entities.ResetPasswordToken token);
+        public Task DeleteAsync(Domain.Entities.ResetPasswordToken token);
+    }
+}
