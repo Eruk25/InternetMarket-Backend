9c653409df1f703f6f201d153f14cf4a30b1ff3a
Danila <danilamatush72@gmail.com>
2026-01-02 20:25:30 +0300

    feat(infrastructure): add AddResetPasswordToken migration

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Migrations/20260102172435_AddResetPasswordToken.cs
@@ -0,0 +1,170 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.UserService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddResetPasswordToken : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tokens_Users_UserId",
+                table: "Tokens");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Tokens",
+                table: "Tokens");
+
+            migrationBuilder.RenameTable(
+                name: "Tokens",
+                newName: "EmailVerificationTokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Tokens_UserId",
+                table: "EmailVerificationTokens",
+                newName: "IX_EmailVerificationTokens_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(500)",
+                maxLength: 500,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(60)",
+                maxLength: 60,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_EmailVerificationTokens",
+                table: "EmailVerificationTokens",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "ResetPasswordTokens",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    ExpiresAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ResetPasswordTokens", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_ResetPasswordTokens_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_Email",
+                table: "Users",
+                column: "Email",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_ResetPasswordTokens_UserId",
+                table: "ResetPasswordTokens",
+                column: "UserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_EmailVerificationTokens_Users_UserId",
+                table: "EmailVerificationTokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_EmailVerificationTokens_Users_UserId",
+                table: "EmailVerificationTokens");
+
+            migrationBuilder.DropTable(
+                name: "ResetPasswordTokens");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Users_Email",
+                table: "Users");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_EmailVerificationTokens",
+                table: "EmailVerificationTokens");
+
+            migrationBuilder.RenameTable(
+                name: "EmailVerificationTokens",
+                newName: "Tokens");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_EmailVerificationTokens_UserId",
+                table: "Tokens",
+                newName: "IX_Tokens_UserId");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Password",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(500)",
+                oldMaxLength: 500);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Email",
+                table: "Users",
+                type: "nvarchar(max)",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(60)",
+                oldMaxLength: 60);
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Tokens",
+                table: "Tokens",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tokens_Users_UserId",
+                table: "Tokens",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
