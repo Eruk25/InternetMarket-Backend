cd5b9706b7098941e08e855c33e0e52e3f4ee3da
Danila <danilamatush72@gmail.com>
2026-01-10 17:49:13 +0300

    feat(presentation): implement IRegistrationEmailVerificationLinkFactory in RegistrationEmailVerificationLinkFactory

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/Implementations/EmailVerificationLinkFactory/RegistrationEmailVerificationLinkFactory.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory;
+using InternetMarket.UserService.Domain.Entities;
+
+namespace InternetMarket.UserService.API.Implementations.EmailVerificationLinkFactory
+{
+    public class RegistrationEmailVerificationLinkFactory : IRegistrationEmailVerificationLinkFactory
+    {
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly LinkGenerator _linkGenerator;
+
+        public RegistrationEmailVerificationLinkFactory(IHttpContextAccessor httpContextAccessor, LinkGenerator linkGenerator)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _linkGenerator = linkGenerator;
+        }
+
+        public string GenerateLink(EmailVerificationToken emailVerificationToken)
+        {
+            string? verificationLink = _linkGenerator.GetUriByAction(
+                action: "VerifyEmail",
+                controller: "Auth",
+                values: new { token = emailVerificationToken.Id },
+                scheme: _httpContextAccessor.HttpContext!.Request.Scheme,
+                host: _httpContextAccessor.HttpContext.Request.Host);
+            return verificationLink ?? throw new Exception("Could not generate link");
+        }
+    }
+}
