c08dda667d3b488b44f65ad864c9f75ffa9f22b7
Danila <danilamatush72@gmail.com>
2026-01-10 17:43:55 +0300

    refactor(application): remove IEmailVerificationLinkFactory

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/EmailVerificationLinkFactory/IEmailVerificationLinkFactory.cs
+++
@@ -1,7 +0,0 @@
-namespace InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory
-{
-    public interface IEmailVerificationLinkFactory
-    {
-        public string GenerateLink(Domain.Entities.EmailVerificationToken token);
-    }
-}
