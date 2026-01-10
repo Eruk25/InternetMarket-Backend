be7e1c959240333515b48f53d495e84ff264b71f
Danila <danilamatush72@gmail.com>
2026-01-10 17:25:16 +0300

    feat(application): implement EmailChangeCommand in EmailChangeCommandHandler

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/EmailVerificationToken/EmailChange/EmailChangeCommandHandler.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.EmailSender;
+using InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory;
+using InternetMarket.UserService.Application.Abstractions.Repositories;
+using InternetMarket.UserService.Application.DTOs.EmailMetadata;
+using MediatR;
+
+namespace InternetMarket.UserService.Application.EmailVerificationToken.EmailChange
+{
+    public class RequestEmailChangeCommandHandler : IRequestHandler<EmailChangeCommand>
+    {
+        private readonly IEmailVerificationTokenRepository _emailVerificationTokenRepository;
+        private readonly IChangeEmailVerificationLinkFactory _emailVerificationLinkFactory;
+        private readonly IEmailService _emailService;
+        private readonly IUserRepository _userRepository;
+
+        public RequestEmailChangeCommandHandler(IEmailVerificationTokenRepository emailVerificationTokenRepository, IUserRepository userRepository,
+            IChangeEmailVerificationLinkFactory emailVerificationLinkFactory, IEmailService emailService)
+        {
+            _emailVerificationTokenRepository = emailVerificationTokenRepository;
+            _emailVerificationLinkFactory = emailVerificationLinkFactory;
+            _emailService = emailService;
+            _userRepository = userRepository;
+        }
+
+        public async Task Handle(EmailChangeCommand request, CancellationToken cancellationToken)
+        {
+            if (request is null)
+                throw new ArgumentNullException(nameof(request));
+
+            var user = await _userRepository.GetByEmailAsync(request.Email);
+
+            if (user is null)
+                throw new InvalidOperationException("User was not found.");
+
+            var token = new Domain.Entities.EmailVerificationToken
+            {
+                Id = Guid.NewGuid(),
+                UserId = user.Id,
+                CreatedAt = DateTime.UtcNow,
+                ExpiresAt = DateTime.UtcNow.AddMinutes(30)
+            };
+            await _emailVerificationTokenRepository.CreateAsync(token);
+            var verificationLink = _emailVerificationLinkFactory.GenerateLink(token);
+            var emailMetadata = new EmailMetadata(user.Email, "From InternetMarket", $"To change email <a href={verificationLink}>click here</a>");
+            await _emailService.SendAsync(emailMetadata);
+        }
+    }
+}
