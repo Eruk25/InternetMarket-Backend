e76a599f780730138ce8ff5f0e80d114e1c2c16b
Danila <danilamatush72@gmail.com>
2026-01-10 17:17:32 +0300

    feat(presentation): add UpdateEmailRequest dto

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.API/DTOs/Requests/UpdateEmail/UpdateEmailRequest.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.UserService.API.DTOs.Requests.UpdateEmail
+{
+    public record UpdateEmailRequest(string Email);
+}
