5d454c8b5a783312dde5172acf4884df62154c79
Danila <danilamatush72@gmail.com>
2026-01-12 14:33:23 +0300

    feat(application): implement GetMyProfileQuery in GetMyProfileQueryHandler

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/GetMe/GetMyProfileQueryHandler.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.UserService.Application.Abstractions.Repositories;
+using MediatR;
+
+namespace InternetMarket.UserService.Application.Users.GetMe
+{
+    public class GetMyProfileQueryHandler : IRequestHandler<GetMyProfileQuery, GetMyProfileResponse>
+    {
+        private readonly IUserRepository _userRepository;
+        
+        public GetMyProfileQueryHandler(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public async Task<GetMyProfileResponse> Handle(GetMyProfileQuery request, CancellationToken cancellationToken)
+        {
+            if(request is null)
+                throw new ArgumentNullException(nameof(request));
+
+            var user = await _userRepository.GetByIdAsync(request.UserId);
+
+            if(user is null)
+                throw new InvalidOperationException($"User with id {request.UserId} not found");
+
+            return new GetMyProfileResponse(user.Id, user.Name, user.Email);
+        }
+    }
+}
