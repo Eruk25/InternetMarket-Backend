259d12975e9fd429e073f5737cdecc831d9bded0
Danila <danilamatush72@gmail.com>
2026-01-20 22:17:35 +0300

    feat(infrastructure): register CartConfiguration in CartContext

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Persistence/DB/CartContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using InternetMarket.CartService.Domain.Entities;
+
+namespace InternetMarket.CartService.Infrastructure;
+
+public class CartContext : DbContext
+{
+    public DbSet<Cart> Carts { get; set; }
+    public DbSet<CartItem> CartItems { get; set; }
+
+    public CartContext(DbContextOptions<CartContext> options) : base(options) { }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfiguration(new CartConfiguration());
+    }
+}
