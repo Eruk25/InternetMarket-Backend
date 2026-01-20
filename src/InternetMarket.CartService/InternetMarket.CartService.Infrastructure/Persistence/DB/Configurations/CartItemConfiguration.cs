e4cd951cfc4f38e58c80265051d38afee29cc908
Danila <danilamatush72@gmail.com>
2026-01-20 22:16:53 +0300

    feat(infrastructure): add CartItemConfiguration

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Persistence/DB/Configurations/CartItemConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using InternetMarket.CartService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace InternetMarket.CartService.Infrastructure;
+
+public class CartItemConfiguration : IEntityTypeConfiguration<CartItem>
+{
+    public void Configure(EntityTypeBuilder<CartItem> builder)
+    {
+        builder.HasKey(ci => ci.Id);
+        builder.Property(ci => ci.CartId)
+            .IsRequired();
+        builder.Property(ci => ci.ProductId)
+            .IsRequired();
+        builder.Property(ci => ci.Quantity)
+            .IsRequired();
+        builder.Property(ci => ci.UntilPrice)
+            .IsRequired();
+    }
+}
