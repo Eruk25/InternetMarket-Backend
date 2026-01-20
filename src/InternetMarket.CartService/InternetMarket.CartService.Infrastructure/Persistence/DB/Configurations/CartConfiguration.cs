f7a1340ed8389e0c920b242e12fece85f4526212
Danila <danilamatush72@gmail.com>
2026-01-20 22:48:47 +0300

    feat(infrastructure): add CartConfiguration

--- src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Persistence/DB/Configurations/CartConfiguration.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Persistence/DB/Configurations/CartConfiguration.cs
@@ -1,6 +1,25 @@
-﻿namespace InternetMarket.CartService.Infrastructure;
+﻿using System.Security.Authentication;
+using InternetMarket.CartService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
-public class CartConfiguration
+namespace InternetMarket.CartService.Infrastructure;
+
+public class CartConfiguration : IEntityTypeConfiguration<Cart>
 {
-
+    public void Configure(EntityTypeBuilder<Cart> builder)
+    {
+        builder.HasKey(c => c.Id);
+        builder.Property(c => c.UserId)
+            .IsRequired();
+        builder.HasMany(c => c.CartItems)
+            .WithOne(ci => ci.Cart)
+            .HasForeignKey(ci => ci.CartId)
+            .OnDelete(DeleteBehavior.Cascade);
+        builder.Property(c => c.CreatedAt)
+            .IsRequired();
+        builder.Property(c => c.UpdatedAt)
+            .IsRequired(false);
+    }
 }
