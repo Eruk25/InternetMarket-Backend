8d20697d9ba16eb66795380614564fdfc05b36de
Danila <danilamatush72@gmail.com>
2026-01-21 13:25:23 +0300

    feat(presentation): register Infrastructure in DI

--- src/InternetMarket.CartService/InternetMarket.CartService.API/Program.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.API/Program.cs
@@ -1,8 +1,11 @@
+using InternetMarket.CartService.Infrastructure.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddInfrastructure(builder.Configuration);
 
 var app = builder.Build();
 
@@ -21,7 +24,7 @@ var summaries = new[]
 
 app.MapGet("/weatherforecast", () =>
 {
-    var forecast =  Enumerable.Range(1, 5).Select(index =>
+    var forecast = Enumerable.Range(1, 5).Select(index =>
         new WeatherForecast
         (
             DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
