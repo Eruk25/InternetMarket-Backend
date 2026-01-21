9ec7116210748ad2e69b42905cf67c7a7bc3a4d7
Danila <danilamatush72@gmail.com>
2026-01-21 11:15:31 +0300

    feat(infrastructure): register CartContext in DI

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.PortableExecutable;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InternetMarket.CartService.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionSection = configuration.GetSection("ConnectionStrings");
+            var connectionString = connectionSection["defaultConnection"];
+            services.AddDbContext<CartContext>(option =>
+                option.UseSqlServer(connectionString));
+
+            return services;
+        }
+    }
+}
