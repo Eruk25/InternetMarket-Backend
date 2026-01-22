11bf88d50224fbd613fe72eb2ac4d8ccc4dbfdd7
Danila <danilamatush72@gmail.com>
2026-01-22 11:37:37 +0300

    refactor(application): change returned type of GetByUserId() to Cart?

--- src/InternetMarket.CartService/InternetMarket.CartService.Application/Abstractions/Repositories/ICartRepository.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Abstractions/Repositories/ICartRepository.cs
@@ -8,7 +8,7 @@ namespace InternetMarket.CartService.Application.Abstractions.Repositories
 {
     public interface ICartRepository
     {
-        public Task<Cart> GetByUserIdAsync(Guid UserId);
+        public Task<Cart?> GetByUserIdAsync(Guid UserId);
         public Task CreateAsync(Cart cart);
         public Task UpdateAsync(Cart cart);
         public Task DeleteAsync(Cart cart);
