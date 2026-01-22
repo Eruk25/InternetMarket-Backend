9abc80e1c3b28f2b00e16eb729a812834423c0f4
Danila <danilamatush72@gmail.com>
2026-01-22 11:26:42 +0300

    feat(application): add ICartRepository

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Abstractions/Repositories/ICartRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.CartService.Domain.Entities;
+
+namespace InternetMarket.CartService.Application.Abstractions.Repositories
+{
+    public interface ICartRepository
+    {
+        public Task<Cart> GetByUserIdAsync(Guid UserId);
+        public Task CreateAsync(Cart cart);
+        public Task UpdateAsync(Cart cart);
+        public Task DeleteAsync(Cart cart);
+    }
+}
