37e37a1c5e133fe710a1cc828404562ac56e2b7f
Danila <danilamatush72@gmail.com>
2026-01-25 23:29:29 +0300

    feat(infrastructure): add CategoryConfiguration

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ProductService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace InternetMarket.ProductService.Infrastructure.Persistance.DB.Configurations
+{
+    public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            builder.HasKey(c => c.Id);
+            builder.Property(c => c.Title)
+                .HasMaxLength(80);
+        }
+    }
+}
