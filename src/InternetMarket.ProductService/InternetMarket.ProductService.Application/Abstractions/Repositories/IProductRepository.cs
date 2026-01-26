a9f983c90b3214111e480e1d6383d195f3990f1c
Danila <danilamatush72@gmail.com>
2026-01-26 11:01:11 +0300

    feat(application): add IProductRepository

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Abstractions/Repositories/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ProductService.Domain.Entities;
+
+namespace InternetMarket.ProductService.Application.Abstractions.Repositories
+{
+    public interface IProductRepository
+    {
+        public Task<IEnumerable<Product>> GetAllAsync();
+        public Task<Product> GetByIdAsync();
+        public Task CreateAsync(Product product);
+        public Task UpdateAsync(Product product);
+        public Task DeleteAsync(Product product);
+    }
+}
