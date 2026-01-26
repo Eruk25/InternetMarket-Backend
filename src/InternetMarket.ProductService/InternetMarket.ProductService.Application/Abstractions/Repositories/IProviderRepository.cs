adb885c2978063f97e0de300c9aa3958f8e99f9b
Danila <danilamatush72@gmail.com>
2026-01-26 12:20:27 +0300

    refactor(infrastructure): change returned parameter of GetById to Provider?

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Abstractions/Repositories/IProviderRepository.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Abstractions/Repositories/IProviderRepository.cs
@@ -9,7 +9,7 @@ namespace InternetMarket.ProductService.Application.Abstractions.Repositories
     public interface IProviderRepository
     {
         public Task<IEnumerable<Provider>> GetAllAsync();
-        public Task<Provider> GetByIdAsync(Guid id);
+        public Task<Provider?> GetByIdAsync(Guid id);
         public Task CreateAsync(Provider provider);
         public Task UpdateAsync(Provider provider);
         public Task DeleteAsync(Provider provider);
