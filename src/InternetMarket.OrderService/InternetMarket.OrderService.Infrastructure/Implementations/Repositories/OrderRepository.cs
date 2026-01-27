c0786155a3c6759e0bcfd12f0a72472980530960
Danila <danilamatush72@gmail.com>
2026-01-27 10:38:02 +0300

    feat(infrastructure): implement IOrderRepository in OrderRepository

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Implementations/Repositories/OrderRepository.cs
@@ -0,0 +1,55 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.OrderService.Application.Abstractions.Repositories;
+using InternetMarket.OrderService.Domain.Entities;
+using InternetMarket.OrderService.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace InternetMarket.OrderService.Infrastructure.Implementations.Repositories
+{
+    public class OrderRepository : IOrderRepository
+    {
+        private readonly OrderContext _context;
+
+        public OrderRepository(OrderContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<Order>> GetAllAsync()
+        {
+            var orders = await _context.Orders
+                .Include(o => o.OrderItems)
+                .ToListAsync();
+            return orders;
+        }
+
+        public async Task<Order?> GetByIdAsync(Guid id)
+        {
+            var order = await _context.Orders
+                .Include(o => o.OrderItems)
+                .FirstOrDefaultAsync(o => o.Id == id);
+            return order;
+        }
+
+        public async Task CreateAsync(Order order)
+        {
+            await _context.Orders.AddAsync(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(Order order)
+        {
+            _context.Orders.Remove(order);
+            await _context.SaveChangesAsync();
+        }
+
+        public Task UpdateAsync(Order order)
+        {
+            _context.Orders.Update(order);
+            return _context.SaveChangesAsync();
+        }
+    }
+}
