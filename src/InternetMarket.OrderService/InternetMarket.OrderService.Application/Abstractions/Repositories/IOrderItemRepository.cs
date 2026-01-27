c053cdef028918a8a9cfa4674b9d3023d58f4ab1
Danila <danilamatush72@gmail.com>
2026-01-27 10:24:44 +0300

    feat(application): add IOrderItemRepository

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Application/Abstractions/Repositories/IOrderItemRepository.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.OrderService.Application.Abstractions.Repositories
+{
+    public interface IOrderItemRepository
+    {
+        Task<IEnumerable<OrderItem>> GetAllAsync();
+        Task<OrderItem?> GetByIdAsync(Guid id);
+        Task CreateAsync(OrderItem orderItem);
+        Task UpdateAsync(OrderItem orderItem);
+        Task DeleteAsync(OrderItem orderItem);
+    }
+}
