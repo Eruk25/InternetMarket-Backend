5b7cc925b9b0bcef7cd37b3aeac4e08498f1d3e0
Danila <danilamatush72@gmail.com>
2026-02-12 17:46:21 +0300

    refactor(application): remove IEmailService using

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Register/RegisterUserCommandHandler.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Users/Register/RegisterUserCommandHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using InternetMarket.UserService.Application.Abstractions.EmailSender;
 using InternetMarket.UserService.Application.Abstractions.EmailVerificationLinkFactory;
 using InternetMarket.UserService.Application.Abstractions.PasswordHasher;
 using InternetMarket.UserService.Application.Abstractions.Repositories;
