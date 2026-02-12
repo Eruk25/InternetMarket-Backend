07d88c98f3f27176e9691fb7e48e364081a1f11f
Danila <danilamatush72@gmail.com>
2026-02-12 17:45:35 +0300

    refactor(application): remove IEmailService from ForgotPasswordCommandHandler

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/ResetPasswordToken/ForgotPasswordCommandHandler.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/ResetPasswordToken/ForgotPasswordCommandHandler.cs
@@ -2,7 +2,6 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using InternetMarket.UserService.Application.Abstractions.EmailSender;
 using InternetMarket.UserService.Application.Abstractions.Repositories;
 using InternetMarket.UserService.Application.Abstractions.ResetPasswordLinkFactory;
 using InternetMarket.UserService.Application.DTOs.EmailMetadata;
@@ -15,15 +14,13 @@ namespace InternetMarket.UserService.Application.ResetPasswordToken
         private readonly IResetPasswordTokenRepository _resetPasswordTokenRepository;
         private readonly IUserRepository _userRepository;
         private readonly IResetPasswordLinkFactory _resetPasswordLinkFactory;
-        private readonly IEmailService _emailService;
 
         public ForgotPasswordCommandHandler(IResetPasswordTokenRepository resetPasswordTokenRepository, IUserRepository userRepository,
-            IResetPasswordLinkFactory resetPasswordLinkFactory, IEmailService emailService)
+            IResetPasswordLinkFactory resetPasswordLinkFactory)
         {
             _resetPasswordTokenRepository = resetPasswordTokenRepository;
             _userRepository = userRepository;
             _resetPasswordLinkFactory = resetPasswordLinkFactory;
-            _emailService = emailService;
         }
 
         public async Task Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
@@ -45,8 +42,6 @@ namespace InternetMarket.UserService.Application.ResetPasswordToken
 
             await _resetPasswordTokenRepository.CreateAsync(token);
             string verificationLink = _resetPasswordLinkFactory.GenerateLink(token);
-            EmailMetadata emailMetadata = new EmailMetadata(user.Email, "From InternetMarket", $"To password reset <a href={verificationLink}>click here</a>");
-            await _emailService.SendAsync(emailMetadata);
         }
     }
 }
