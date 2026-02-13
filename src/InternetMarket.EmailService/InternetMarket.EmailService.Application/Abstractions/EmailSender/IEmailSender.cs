28b55280ed318c241e247bd9f2c9694f70aff2a6
Danila <danilamatush72@gmail.com>
2026-02-13 09:24:52 +0300

    refactor(application): change parameters of method to EmailMetaData

--- src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Abstractions/EmailSender/IEmailSender.cs
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Abstractions/EmailSender/IEmailSender.cs
@@ -2,11 +2,12 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.EmailService.Application.DTOs.EmailMetadata;
 
 namespace InternetMarket.EmailService.Application.Abstractions.EmailSender
 {
     public interface IEmailSender
     {
-        Task SendAsync(string to, string subject, string body);
+        Task SendAsync(EmailMetadata emailMetadata);
     }
 }
