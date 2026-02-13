81600a8b2c4b99090ca75fbc021900b57ef99901
Danila <danilamatush72@gmail.com>
2026-02-13 13:44:15 +0300

    feat(application): add UserRegistredConsumer

---
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Consumers/UserRegisteredConsumer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.EmailService.Application.Abstractions.EmailSender;
+using InternetMarket.EmailService.Application.DTOs.EmailMetadata;
+using InternetMarket.Contracts.Events;
+using MassTransit;
+
+namespace InternetMarket.EmailService.Application.Consumers
+{
+    public class UserRegisteredConsumer : IConsumer<UserRegistered>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public UserRegisteredConsumer(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<UserRegistered> context)
+        {
+            EmailMetadata emailMetadata = new EmailMetadata(
+                context.Message.Email,
+                "Hello! From InternetMerket",
+                $"""To verify your email click <a src="{context.Message.VerificationLink}">here</a>"""
+            );
+            await _emailSender.SendAsync(emailMetadata);
+        }
+    }
+}
