88e738141b5ed2c52c9ee3ed36fa1ac0fc8ef31f
Danila <danilamatush72@gmail.com>
2026-02-13 10:05:16 +0300

    feat(presentation): register FluentEmail in DI

--- src/InternetMarket.EmailService/InternetMarket.EmailService.API/Program.cs
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.API/Program.cs
@@ -1,11 +1,14 @@
 using InternetMarket.EmailService.Application.Extensions;
 using InternetMarket.EmailService.Infrastructure.Extensions.DI;
+using InternetMarket.EmailService.Infrastructure.Extensions.FluentEmail;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
+builder.Services
+    .AddFluentEmail(builder.Configuration);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
