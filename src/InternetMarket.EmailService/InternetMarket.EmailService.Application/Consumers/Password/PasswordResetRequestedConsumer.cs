ce4eb9314cf855cb1ded6523667a78edce571ac9
Danila <danilamatush72@gmail.com>
2026-02-13 20:05:02 +0300

    feat(application): add PasswordResetRequestedConsumer

---
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Consumers/Password/PasswordResetRequestedConsumer.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.Contracts.Events.Password;
+using InternetMarket.EmailService.Application.Abstractions.EmailSender;
+using InternetMarket.EmailService.Application.DTOs.EmailMetadata;
+using MassTransit;
+
+namespace InternetMarket.EmailService.Application.Consumers.Password
+{
+    public class PasswordResetRequestedConsumer : IConsumer<PasswordResetRequested>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public PasswordResetRequestedConsumer(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<PasswordResetRequested> context)
+        {
+            EmailMetadata emailMetadata = new EmailMetadata(
+                context.Message.Email,
+                "Reset password. InternetMarket",
+                $"""For password reseting click <a src="{context.Message.ResetLink}">here</a>"""
+            );
+            await _emailSender.SendAsync(emailMetadata);
+        }
+    }
+}
