bd3af40c996c50af88b08fffa5922bc5838eb213
Danila <danilamatush72@gmail.com>
2026-02-13 14:02:11 +0300

    refactor(application): remove Events

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Events/UserRegistered.cs
+++
@@ -1,9 +0,0 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace InternetMarket.UserService.Application.Events
-{
-    public record UserRegistered(string Email, string Name, string VerificationLink);
-}
