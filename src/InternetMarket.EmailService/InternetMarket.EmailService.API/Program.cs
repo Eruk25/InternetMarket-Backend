8884d9af2f47cb992b465a728aac9173e568cf4c
Danila <danilamatush72@gmail.com>
2026-02-13 10:02:45 +0300

    feat(presentation): register Application and Infrastructure in DI

--- src/InternetMarket.EmailService/InternetMarket.EmailService.API/Program.cs
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.API/Program.cs
@@ -1,5 +1,11 @@
+using InternetMarket.EmailService.Application.Extensions;
+using InternetMarket.EmailService.Infrastructure.Extensions.DI;
+
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services
+    .AddApplication()
+    .AddInfrastructure(builder.Configuration);
 builder.Services.AddOpenApi();
 
 var app = builder.Build();
