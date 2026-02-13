ae9acaee6bb329f0df2945eb0939c5383ff066d7
Danila <danilamatush72@gmail.com>
2026-02-13 20:07:33 +0300

    feat(application): use MassTransit for publish PasswordResetRequested event

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/ResetPasswordToken/ForgotPasswordCommandHandler.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/ResetPasswordToken/ForgotPasswordCommandHandler.cs
@@ -2,24 +2,28 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.Contracts.Events.Password;
 using InternetMarket.UserService.Application.Abstractions.Repositories;
 using InternetMarket.UserService.Application.Abstractions.ResetPasswordLinkFactory;
+using MassTransit;
 using MediatR;
 
 namespace InternetMarket.UserService.Application.ResetPasswordToken
 {
     public class ForgotPasswordCommandHandler : IRequestHandler<ForgotPasswordCommand>
     {
+        private readonly IPublishEndpoint _publishEndpoint;
         private readonly IResetPasswordTokenRepository _resetPasswordTokenRepository;
         private readonly IUserRepository _userRepository;
         private readonly IResetPasswordLinkFactory _resetPasswordLinkFactory;
 
         public ForgotPasswordCommandHandler(IResetPasswordTokenRepository resetPasswordTokenRepository, IUserRepository userRepository,
-            IResetPasswordLinkFactory resetPasswordLinkFactory)
+            IResetPasswordLinkFactory resetPasswordLinkFactory, IPublishEndpoint publishEndpoint)
         {
             _resetPasswordTokenRepository = resetPasswordTokenRepository;
             _userRepository = userRepository;
             _resetPasswordLinkFactory = resetPasswordLinkFactory;
+            _publishEndpoint = publishEndpoint;
         }
 
         public async Task Handle(ForgotPasswordCommand request, CancellationToken cancellationToken)
@@ -36,11 +40,16 @@ namespace InternetMarket.UserService.Application.ResetPasswordToken
             {
                 Id = Guid.NewGuid(),
                 UserId = user.Id,
-                ExpiresAt = DateTime.UtcNow
+                ExpiresAt = DateTime.UtcNow.AddMinutes(30),
             };
 
             await _resetPasswordTokenRepository.CreateAsync(token);
             string verificationLink = _resetPasswordLinkFactory.GenerateLink(token);
+            await _publishEndpoint.Publish(new PasswordResetRequested
+            {
+                Email = user.Email,
+                ResetLink = verificationLink
+            });
         }
     }
 }
