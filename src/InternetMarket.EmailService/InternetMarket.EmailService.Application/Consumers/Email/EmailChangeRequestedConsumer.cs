72c18bbc2bb33298f328cf89e7a48b212dddc10b
Danila <danilamatush72@gmail.com>
2026-02-16 12:00:32 +0300

    feat(application): add EmailChangeRequestedConsumer

---
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Consumers/Email/EmailChangeRequestedConsumer.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.Contracts.Events.Email;
+using InternetMarket.EmailService.Application.Abstractions.EmailSender;
+using InternetMarket.EmailService.Application.DTOs.EmailMetadata;
+using MassTransit;
+
+namespace InternetMarket.EmailService.Application.Consumers.Email
+{
+    public class EmailChangeRequestedConsumer : IConsumer<EmailChangeRequested>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public EmailChangeRequestedConsumer(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<EmailChangeRequested> context)
+        {
+            EmailMetadata emailMetadata = new EmailMetadata(
+                context.Message.Email,
+                "Change Email. InternetMarket",
+                $"""For email changing click <a src="{context.Message.VerificationLink}">here</a>""");
+            await _emailSender.SendAsync(emailMetadata);
+        }
+    }
+}
