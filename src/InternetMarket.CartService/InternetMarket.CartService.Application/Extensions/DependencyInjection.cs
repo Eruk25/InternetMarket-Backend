4e273e29d90a856f5151f1ae940e74fc8d6cc808
Danila <danilamatush72@gmail.com>
2026-02-16 14:56:17 +0300

    feat(application): register MassTransit in DI

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Extensions/DependencyInjection.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InternetMarket.CartService.Application.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            services.AddMassTransit(x =>
+            {
+                x.UsingRabbitMq((context, cfg) =>
+                {
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+            return services;
+        }
+    }
+}
