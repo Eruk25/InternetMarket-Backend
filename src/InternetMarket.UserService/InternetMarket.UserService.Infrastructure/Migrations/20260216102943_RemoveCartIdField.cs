66ccedf0d72c2b9737553a215c55bea8335134ca
Danila <danilamatush72@gmail.com>
2026-02-16 13:34:23 +0300

    refactor(infrastructure): add RemoveCardIdField migration

---
+++ src/InternetMarket.UserService/InternetMarket.UserService.Infrastructure/Migrations/20260216102943_RemoveCartIdField.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.UserService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveCartIdField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CartId",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CartId",
+                table: "Users",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+    }
+}
