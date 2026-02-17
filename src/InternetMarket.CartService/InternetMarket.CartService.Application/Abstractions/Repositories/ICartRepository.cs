ce6853d3d9017237691e8b98e466d23d2b26a133
Danila <danilamatush72@gmail.com>
2026-02-17 15:49:08 +0300

    feat(application): add ExistebyUserIdAsync method

--- src/InternetMarket.CartService/InternetMarket.CartService.Application/Abstractions/Repositories/ICartRepository.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Abstractions/Repositories/ICartRepository.cs
@@ -9,6 +9,7 @@ namespace InternetMarket.CartService.Application.Abstractions.Repositories
     public interface ICartRepository
     {
         public Task<Cart?> GetByUserIdAsync(Guid UserId);
+        public Task<bool> ExisteByUserIdAsync(Guid UserId);
         public Task CreateAsync(Cart cart);
         public Task UpdateAsync(Cart cart);
         public Task DeleteAsync(Cart cart);
