d251dfad8dce7d6361846d4cdde3275f6ea932f6
Danila <danilamatush72@gmail.com>
2026-02-17 16:53:19 +0300

    feat(application): add CartDto

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Carts/CartDto.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.CartService.Application.CartItems;
+using InternetMarket.CartService.Domain.Entities;
+
+namespace InternetMarket.CartService.Application.Carts
+{
+    public record CartDto(
+        IEnumerable<CartItemDto> CartItems
+    );
+}
