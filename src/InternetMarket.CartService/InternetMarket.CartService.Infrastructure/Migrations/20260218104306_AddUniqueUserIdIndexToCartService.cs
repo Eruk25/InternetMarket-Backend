5dcbf9f672007b36a7faeedffbeba11ff8208123
Danila <danilamatush72@gmail.com>
2026-02-18 13:48:29 +0300

    feat(infrastructure): add AddUniqueUserIdIndexToCartService migration

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Migrations/20260218104306_AddUniqueUserIdIndexToCartService.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.CartService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUniqueUserIdIndexToCartService : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts",
+                column: "UserId",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_Carts_UserId",
+                table: "Carts");
+        }
+    }
+}
