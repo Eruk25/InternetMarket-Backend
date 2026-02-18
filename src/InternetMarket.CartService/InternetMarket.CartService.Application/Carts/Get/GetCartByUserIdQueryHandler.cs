fb59397f97f4b358866519dd88e9c6bbb937819f
Danila <danilamatush72@gmail.com>
2026-02-18 09:41:28 +0300

    feat(application): add GetCartByUserIdQueryHandler

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Carts/Get/GetCartByUserIdQueryHandler.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.CartService.Application.Abstractions.Repositories;
+using InternetMarket.CartService.Application.CartItems;
+using InternetMarket.CartService.Domain.Entities;
+using MediatR;
+
+namespace InternetMarket.CartService.Application.Carts.Get
+{
+    public class GetCartByUserIdQueryHandler : IRequestHandler<GetCartByUserIdQuery, CartDto>
+    {
+        private readonly ICartRepository _cartRepository;
+
+        public GetCartByUserIdQueryHandler(ICartRepository cartRepository)
+        {
+            _cartRepository = cartRepository;
+        }
+
+        public async Task<CartDto> Handle(GetCartByUserIdQuery request, CancellationToken cancellationToken)
+        {
+            var cart = await _cartRepository.GetByUserIdAsync(request.UserId);
+            if (cart is null)
+            {
+                cart = new Cart(request.UserId);
+                await _cartRepository.CreateAsync(cart);
+            }
+
+            var cartItems = cart.CartItems
+                .Select(ci => new CartItemDto(
+                    ci.ProductId,
+                    ci.Quantity,
+                    ci.UntilPrice))
+                .ToList();
+            return new CartDto(cartItems);
+        }
+    }
+}
