324fb62e2a11d776eaa5f35905e0278d00cad598
Danila <danilamatush72@gmail.com>
2026-02-20 15:33:41 +0300

    feat(application): add GetProductsByIdsQueryHandler

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Products/Get/GetByIds/GetProductsByIdsQueryHandler.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ProductService.Application.Abstractions.Repositories;
+using MediatR;
+
+namespace InternetMarket.ProductService.Application.Products.Get.GetByIds
+{
+    public class GetProductsByIdsQueryHandler : IRequestHandler<GetProductsByIdsQuery, IEnumerable<ProductDto>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductsByIdsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> Handle(GetProductsByIdsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetByIdsAsync(request.Ids);
+
+            return products.Select(p => new ProductDto(
+                p.Id,
+                p.Title,
+                p.Description,
+                p.Price,
+                p.Quantity,
+                p.Category!.Title,
+                p.Provider!.Name
+            ));
+        }
+    }
+}
