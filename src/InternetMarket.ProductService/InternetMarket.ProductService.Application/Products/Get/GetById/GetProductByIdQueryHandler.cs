fd3d7fb352e7f47fb14cd845f26594da8e569c1f
Danila <danilamatush72@gmail.com>
2026-02-20 15:33:14 +0300

    feat(application): add GetProductByIdQueryHandler

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Products/Get/GetById/GetProductByIdQueryHandler.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ProductService.Application.Abstractions.Repositories;
+using MediatR;
+
+namespace InternetMarket.ProductService.Application.Products.Get.GetById
+{
+    public class GetProductByIdQueryHandler : IRequestHandler<GetProductByIdQuery, ProductDto>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetProductByIdQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<ProductDto> Handle(GetProductByIdQuery request, CancellationToken cancellationToken)
+        {
+            var product = await _productRepository.GetByIdAsync(request.Id);
+
+            if (product is null)
+                throw new ArgumentNullException($"Product with id {request.Id} was not founded.");
+
+            return new ProductDto(
+                product.Id,
+                product.Title,
+                product.Description,
+                product.Price,
+                product.Quantity,
+                product.Category!.Title,
+                product.Provider!.Name);
+        }
+    }
+}
