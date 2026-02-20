7988fdffd6b2aa739d4562273fe010f91d4c6789
Danila <danilamatush72@gmail.com>
2026-02-20 15:32:06 +0300

    feat(application): add GetAllProductsQueryHandler

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Application/Products/Get/GetAll/GetAllProductsQueryHandler.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ProductService.Application.Abstractions.Repositories;
+using MediatR;
+
+namespace InternetMarket.ProductService.Application.Products.Get
+{
+    public class GetAllProductsQueryHandler : IRequestHandler<GetAllProductsQuery, IEnumerable<ProductDto>>
+    {
+        private readonly IProductRepository _productRepository;
+
+        public GetAllProductsQueryHandler(IProductRepository productRepository)
+        {
+            _productRepository = productRepository;
+        }
+
+        public async Task<IEnumerable<ProductDto>> Handle(GetAllProductsQuery request, CancellationToken cancellationToken)
+        {
+            var products = await _productRepository.GetAllAsync();
+
+            return products.Select(p =>
+            new ProductDto(
+                p.Id,
+                p.Title,
+                p.Description,
+                p.Price,
+                p.Quantity,
+                p.Category!.Title,
+                p.Provider!.Name))
+                .ToList();
+        }
+    }
+}
