8e6cfc8e9128574c8b39984fe39b6d171f36b000
Danila <danilamatush72@gmail.com>
2026-03-19 14:23:22 +0300

    feat(infrastructure): add AddTitleFieldToCartItem migration

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Migrations/20260319112227_AddTitleFieldToCartItem.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.CartService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTitleFieldToCartItem : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UntilPrice",
+                table: "CartItems",
+                newName: "Price");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "CartItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "CartItems");
+
+            migrationBuilder.RenameColumn(
+                name: "Price",
+                table: "CartItems",
+                newName: "UntilPrice");
+        }
+    }
+}
