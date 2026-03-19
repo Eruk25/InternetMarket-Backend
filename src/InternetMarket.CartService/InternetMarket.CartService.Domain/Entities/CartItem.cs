36a9c3eb4fa925b41016ba05f46042a4f73cc60e
Danila <danilamatush72@gmail.com>
2026-03-19 12:18:41 +0300

    refactor(domain): change of field name from UntilPrice to Price and add to ctor

--- src/InternetMarket.CartService/InternetMarket.CartService.Domain/Entities/CartItem.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Domain/Entities/CartItem.cs
@@ -14,13 +14,14 @@ namespace InternetMarket.CartService.Domain.Entities
         public Cart? Cart { get; set; }
         public Guid ProductId { get; set; }
         public string ProductName { get; set; }
-        public decimal UntilPrice { get; set; }
+        public decimal Price { get; set; }
         public int Quantity { get; set; }
 
-        public CartItem(Guid productId, string productName, int quantity)
+        public CartItem(Guid productId, string productName, decimal price, int quantity)
         {
             ProductId = productId;
             ProductName = productName;
+            Price = price;
             Quantity = quantity;
         }
     }
