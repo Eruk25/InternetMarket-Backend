3dbe4634cb11b9cd0321ac1668b4e80ab71c45f6
Danila <danilamatush72@gmail.com>
2026-03-19 14:20:24 +0300

    feat(application): use IProductServiceClient

--- src/InternetMarket.CartService/InternetMarket.CartService.Application/CartItems/Create/AddCartItemCommandHandler.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/CartItems/Create/AddCartItemCommandHandler.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.CartService.Application.Abstractions.Clients;
 using InternetMarket.CartService.Application.Abstractions.Repositories;
 using InternetMarket.CartService.Domain.Entities;
 using MediatR;
@@ -11,14 +12,21 @@ namespace InternetMarket.CartService.Application.CartItems.Create
     public class AddCartItemCommandHandler : IRequestHandler<AddCartItemCommand>
     {
         private readonly ICartRepository _cartRepository;
+        private readonly IProductServiceClient _productServiceClient;
 
-        public AddCartItemCommandHandler(ICartRepository cartRepository)
+        public AddCartItemCommandHandler(ICartRepository cartRepository, IProductServiceClient productServiceClient)
         {
             _cartRepository = cartRepository;
+            _productServiceClient = productServiceClient;
         }
 
         public async Task Handle(AddCartItemCommand request, CancellationToken cancellationToken)
         {
+            var product = await _productServiceClient.GetProductByIdAsync(request.ProductId);
+
+            if (product is null)
+                throw new Exception("Product was not found");
+
             var cart = await _cartRepository.GetByUserIdAsync(request.UserId);
 
             if (cart is null)
@@ -27,7 +35,7 @@ namespace InternetMarket.CartService.Application.CartItems.Create
                 await _cartRepository.CreateAsync(cart);
             }
 
-            cart.AddItem(request.ProductId, request.Quantity);
+            cart.AddItem(product.Id, product.Title, product.Price, request.Quantity);
             await _cartRepository.UpdateAsync(cart);
         }
     }
