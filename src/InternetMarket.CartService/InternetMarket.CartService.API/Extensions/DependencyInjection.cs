717d9265464e91e7fb773f1e7396778c9911a005
Danila <danilamatush72@gmail.com>
2026-03-20 11:35:09 +0300

    feat(presentation): register JwtBearer AuthenticationSheme in DI

--- src/InternetMarket.CartService/InternetMarket.CartService.API/Extensions/DependencyInjection.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.API/Extensions/DependencyInjection.cs
@@ -1,14 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace InternetMarket.CartService.API.Extensions
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
         {
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+
+                        ValidIssuer = configuration[""],
+                        ValidAudience = configuration[""],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(configuration[""]!)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+                });
             services.AddControllers();
             return services;
         }
