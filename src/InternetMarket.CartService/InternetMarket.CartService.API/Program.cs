c40f1b57c2ab1d6210cd0da97059fcf440095163
Danila <danilamatush72@gmail.com>
2026-03-20 11:37:48 +0300

    feat(presentation): pass a configuration to Presentation DI

--- src/InternetMarket.CartService/InternetMarket.CartService.API/Program.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.API/Program.cs
@@ -6,7 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services
-    .AddPresentation()
+    .AddPresentation(builder.Configuration)
     .AddApplication()
     .AddInfrastructure(builder.Configuration);
 
