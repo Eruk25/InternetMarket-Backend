97b617f65f8e11d19762548bf82289471a7fb1e7
Danila <danilamatush72@gmail.com>
2026-03-21 11:51:33 +0300

    feat(application): add CreateOrderCommandHandler

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Application/Orders/Create/CreateOrderCommandHandler.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.OrderService.Application.Abstractions.Clients;
+using InternetMarket.OrderService.Application.Abstractions.Repositories;
+using InternetMarket.OrderService.Domain.Entities;
+using MediatR;
+
+namespace InternetMarket.OrderService.Application.Orders.Create
+{
+    public class CreateOrderCommandHandler : IRequestHandler<CreateOrderCommand>
+    {
+        private readonly IOrderRepository _orderRepository;
+        private readonly ICartServiceClient _cartClient;
+
+        public CreateOrderCommandHandler(IOrderRepository orderRepository, ICartServiceClient cartClient)
+        {
+            _orderRepository = orderRepository;
+            _cartClient = cartClient;
+        }
+
+        public async Task Handle(CreateOrderCommand request, CancellationToken cancellationToken)
+        {
+            var cart = await _cartClient.GetCartAsync(request.UserId);
+
+            if (cart is null || !cart.CartItems.Any())
+                throw new Exception("Cart is empty");
+
+            var orderItems = cart.CartItems
+                .Select(ci => new OrderItem(
+                    ci.ProductId,
+                    ci.Quantity,
+                    ci.Price));
+
+            var order = new Order(request.UserId);
+            order.AddItems(orderItems);
+
+            await _orderRepository.CreateAsync(order);
+        }
+    }
+}
