2d4f2becec6cee6ef0670959d4c489137ad91388
Danila <danilamatush72@gmail.com>
2026-03-22 10:50:04 +0300

    feat(infrastructure): add ChangeIsNullableToFalseOnPaymentDateField migration

--- src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260321113033_ChangeIsNullableToFalseOnPaymentDateField.Designer.cs
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260321113033_ChangeIsNullableToFalseOnPaymentDateField.Designer.cs
@@ -4,6 +4,7 @@ using InternetMarket.OrderService.Infrastructure.Persistence;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace InternetMarket.OrderService.Infrastructure.Migrations
 {
     [DbContext(typeof(OrderContext))]
-    partial class OrderContextModelSnapshot : ModelSnapshot
+    [Migration("20260321113033_ChangeIsNullableToFalseOnPaymentDateField")]
+    partial class ChangeIsNullableToFalseOnPaymentDateField
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
