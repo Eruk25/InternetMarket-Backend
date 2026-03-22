1e972b712f3fffc22d64443e8346c4cb02eb7a55
Danila <danilamatush72@gmail.com>
2026-03-22 10:51:41 +0300

    feat(presentation): add DependencyInjection extension for Presentation layer

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.API/Extensions/DependencyInjection.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.OrderService.API.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        {
+            services.AddControllers();
+
+            return services;
+        }
+    }
+}
