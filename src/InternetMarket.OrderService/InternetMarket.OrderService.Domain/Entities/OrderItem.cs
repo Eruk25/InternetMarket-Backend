0eeae51c0aac566a82ae1a846b9914e508031b16
Danila <danilamatush72@gmail.com>
2026-03-24 12:53:41 +0300

    refactor(domain): add Title, TotalPrice, UnitPrice fields

--- src/InternetMarket.OrderService/InternetMarket.OrderService.Domain/Entities/OrderItem.cs
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Domain/Entities/OrderItem.cs
@@ -8,19 +8,22 @@ namespace InternetMarket.OrderService.Domain.Entities
     public class OrderItem
     {
         public Guid Id { get; set; }
+        public string Title { get; set; }
+        public int Quantity { get; set; }
+        public decimal UnitPrice { get; set; }
+        public decimal TotalPrice => Quantity * UnitPrice;
         public Guid ProductId { get; set; }
         public Guid OrderId { get; set; }
         public Order? Order { get; set; }
-        public int Quantity { get; set; }
-        public decimal UntilPrice { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime? UpdatedAt { get; set; }
 
-        public OrderItem(Guid productId, int quantity, decimal untilPrice)
+        public OrderItem(Guid productId, string title, int quantity, decimal unitPrice)
         {
             ProductId = productId;
+            Title = title;
             Quantity = quantity;
-            UntilPrice = untilPrice;
+            UnitPrice = unitPrice;
             CreatedAt = DateTime.UtcNow;
         }
     }
