c707560e42b8c4833c008bceeefcc84e22baa502
Danila <danilamatush72@gmail.com>
2026-03-24 12:51:27 +0300

    feat(infrastructure): add ChangeNameOfFieldFromUntilPriceToUnitPrice migration

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260324094455_ChangeNameOfFieldFromUntilPriceToUnitPrice.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNameOfFieldFromUntilPriceToUnitPrice : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UntilPrice",
+                table: "OrderItems",
+                newName: "UnitPrice");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Title",
+                table: "OrderItems",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Title",
+                table: "OrderItems");
+
+            migrationBuilder.RenameColumn(
+                name: "UnitPrice",
+                table: "OrderItems",
+                newName: "UntilPrice");
+        }
+    }
+}
