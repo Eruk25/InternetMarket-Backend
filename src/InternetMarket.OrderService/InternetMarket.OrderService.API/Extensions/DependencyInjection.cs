ecde7f9700af04afed774c30f86b5086dfdf47fa
Danila <danilamatush72@gmail.com>
2026-03-25 09:41:54 +0300

    feat(presentation): register JwtAuthScheme in DI

--- src/InternetMarket.OrderService/InternetMarket.OrderService.API/Extensions/DependencyInjection.cs
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.API/Extensions/DependencyInjection.cs
@@ -1,16 +1,35 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
 
 namespace InternetMarket.OrderService.API.Extensions
 {
     public static class DependencyInjection
     {
-        public static IServiceCollection AddPresentation(this IServiceCollection services)
+        public static IServiceCollection AddPresentation(this IServiceCollection services, IConfiguration configuration)
         {
-            services.AddControllers();
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateIssuerSigningKey = true,
 
+                        ValidIssuer = configuration["AuthOptions:Issuer"],
+                        ValidAudience = configuration["AuthOptions:Audience"],
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                         Encoding.UTF8.GetBytes(configuration["AuthOptions:SecretKey"]!)),
+                        RoleClaimType = ClaimTypes.Role
+                    };
+                });
+            services.AddControllers();
             return services;
         }
     }
