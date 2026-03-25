f27e1c15516c01b35adc1712adf3acb81353b500
Danila <danilamatush72@gmail.com>
2026-03-25 12:01:54 +0300

    feat(infrastructure): add AddTotalPriceFieldToOrderModel migration

---
+++ src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260325075924_AddTotalPriceFieldToOrderModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTotalPriceFieldToOrderModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "TotalPrice",
+                table: "Orders",
+                type: "decimal(18,2)",
+                nullable: false,
+                defaultValue: 0m);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TotalPrice",
+                table: "Orders");
+        }
+    }
+}
