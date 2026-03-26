b263bd376e186c5c37c764982e8e3a45c5726fb7
Danila <danilamatush72@gmail.com>
2026-03-26 12:53:14 +0300

    feat(application): add OrderCancelledConsumer

---
+++ src/InternetMarket.EmailService/InternetMarket.EmailService.Application/Consumers/Order/OrderCancelledConsumer.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.Contracts.Events.Order;
+using InternetMarket.EmailService.Application.Abstractions.EmailSender;
+using InternetMarket.EmailService.Application.DTOs.EmailMetadata;
+using MassTransit;
+
+namespace InternetMarket.EmailService.Application.Consumers.Order
+{
+    public class OrderCancelledConsumer : IConsumer<OrderCancelled>
+    {
+        private readonly IEmailSender _emailSender;
+
+        public OrderCancelledConsumer(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        public async Task Consume(ConsumeContext<OrderCancelled> context)
+        {
+            EmailMetadata emailMetadata = new EmailMetadata(
+                context.Message.Email,
+                $"Заказ №{context.Message.OrderId} был отменен.",
+                $"Something must be"
+            );
+
+            await _emailSender.SendAsync(emailMetadata);
+        }
+    }
+}
