9be29de3803e4cf1b55a1300faae7198e5f84279
Danila <danilamatush72@gmail.com>
2026-03-31 22:13:39 +0300

    refactor(infrastractur): add ChangeNamesOfFieldsFromTitleToProductNameAndTitleToCategoryName migration

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Migrations/20260331191116_ChangeNamesOfFieldsFromTitleToProductNameAndTitleToCategoryName.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.ProductService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNamesOfFieldsFromTitleToProductNameAndTitleToCategoryName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Products",
+                newName: "ProductName");
+
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "Categories",
+                newName: "CategoryName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "Products",
+                newName: "Title");
+
+            migrationBuilder.RenameColumn(
+                name: "CategoryName",
+                table: "Categories",
+                newName: "Title");
+        }
+    }
+}
