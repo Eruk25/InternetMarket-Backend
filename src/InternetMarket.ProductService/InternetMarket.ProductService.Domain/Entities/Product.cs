90396824a0b7ea91d2383dda9d084ab40126d192
Danila <danilamatush72@gmail.com>
2026-03-31 22:07:18 +0300

    refactor(domain): change name of field from Title to ProductName

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Product.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Product.cs
@@ -9,7 +9,7 @@ namespace InternetMarket.ProductService.Domain.Entities
     public class Product
     {
         public Guid Id { get; private set; }
-        public string Title { get; private set; }
+        public string ProductName { get; private set; }
         public string Description { get; private set; }
         public decimal Price { get; private set; }
         public int Quantity { get; private set; }
@@ -18,9 +18,9 @@ namespace InternetMarket.ProductService.Domain.Entities
         public Guid ProviderId { get; private set; }
         public Provider? Provider { get; private set; }
 
-        public Product(string title, string description, decimal price, int quantity, Guid categoryId, Guid providerId)
+        public Product(string productName, string description, decimal price, int quantity, Guid categoryId, Guid providerId)
         {
-            Title = title;
+            ProductName = productName;
             Description = description;
             Price = price;
             Quantity = quantity;
