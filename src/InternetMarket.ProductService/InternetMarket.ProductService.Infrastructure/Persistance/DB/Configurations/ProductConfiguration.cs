3d2f72a48304acb73e376126528a8e538e6d92bd
Danila <danilamatush72@gmail.com>
2026-04-01 19:57:48 +0300

    feat(infrastructure): add conversions for ProductName, Description, Quantity, Price value objects

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/ProductConfiguration.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/ProductConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InternetMarket.ProductService.Domain.Entities;
+using InternetMarket.ProductService.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -14,11 +15,25 @@ namespace InternetMarket.ProductService.Infrastructure.Persistance.DB.Configurat
         {
             builder.HasKey(p => p.Id);
             builder.Property(p => p.ProductName)
+                .HasConversion(
+                    productName => productName.Value,
+                    value => ProductName.Create(value))
                 .HasMaxLength(60);
             builder.Property(p => p.Description)
+                .HasConversion(
+                    description => description.Value,
+                    value => Description.Create(value))
                 .HasMaxLength(550);
-            builder.Property(p => p.Quantity);
-            builder.Property(p => p.Price);
+            builder.Property(p => p.Quantity)
+                .HasConversion(
+                    quantity => quantity.Value,
+                    value => Quantity.Create(value))
+                .IsRequired();
+            builder.Property(p => p.Price)
+                .HasConversion(
+                    price => price.Value,
+                    value => Price.Create(value))
+                .IsRequired();
             builder.HasOne(p => p.Provider)
                 .WithMany()
                 .HasForeignKey(p => p.ProviderId);
