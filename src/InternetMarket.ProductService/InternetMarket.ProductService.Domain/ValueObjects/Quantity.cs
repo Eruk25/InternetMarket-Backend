7b759378700293fd7c4aa0fe3cdb0c9c08204265
Danila <danilamatush72@gmail.com>
2026-04-01 19:47:15 +0300

    feat(domain): add Quantity vo

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/ValueObjects/Quantity.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.ProductService.Domain.ValueObjects
+{
+    public class Quantity
+    {
+        public int Value { get; }
+
+        private Quantity(int value) => Value = value;
+
+        public static Quantity Create(int value)
+        {
+            if (value <= 0 || int.MaxValue < value)
+                throw new ArgumentException($"Quantity cannot be equal or less then 0 or more {int.MaxValue}");
+
+            return new Quantity(value);
+        }
+    }
+}
