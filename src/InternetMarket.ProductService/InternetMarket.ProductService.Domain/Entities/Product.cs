31af9d007dd340504e86548faccb165198fbb74b
Danila <danilamatush72@gmail.com>
2026-04-01 19:48:04 +0300

    refactor(domain): use ProductName, Description, Price, Quantity value objects

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Product.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Product.cs
@@ -3,22 +3,23 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.ProductService.Domain.ValueObjects;
 
 namespace InternetMarket.ProductService.Domain.Entities
 {
     public class Product
     {
         public Guid Id { get; private set; }
-        public string ProductName { get; private set; }
-        public string Description { get; private set; }
-        public decimal Price { get; private set; }
-        public int Quantity { get; private set; }
+        public ProductName ProductName { get; private set; }
+        public Description Description { get; private set; }
+        public Price Price { get; private set; }
+        public Quantity Quantity { get; private set; }
         public Guid CategoryId { get; private set; }
         public Category? Category { get; private set; }
         public Guid ProviderId { get; private set; }
         public Provider? Provider { get; private set; }
 
-        public Product(string productName, string description, decimal price, int quantity, Guid categoryId, Guid providerId)
+        public Product(ProductName productName, Description description, Price price, Quantity quantity, Guid categoryId, Guid providerId)
         {
             ProductName = productName;
             Description = description;
