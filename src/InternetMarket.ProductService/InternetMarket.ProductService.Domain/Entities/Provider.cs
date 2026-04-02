8d8734770efb17999a1951a18cbb197bcfe04ef1
Danila <danilamatush72@gmail.com>
2026-04-02 12:25:58 +0300

    refactor(domain): use ProviderName, Address, Email, NumberPhone vo

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Provider.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Provider.cs
@@ -4,18 +4,19 @@ using System.Collections.Specialized;
 using System.Linq;
 using System.Reflection.Metadata.Ecma335;
 using System.Threading.Tasks;
+using InternetMarket.ProductService.Domain.ValueObjects;
 
 namespace InternetMarket.ProductService.Domain.Entities
 {
     public class Provider
     {
         public Guid Id { get; private set; }
-        public string Name { get; private set; }
-        public string Address { get; private set; }
-        public string Email { get; private set; }
-        public string NumberPhone { get; private set; }
+        public ProviderName Name { get; private set; }
+        public Address Address { get; private set; }
+        public Email Email { get; private set; }
+        public NumberPhone NumberPhone { get; private set; }
 
-        public Provider(string name, string address, string email, string numberPhone)
+        public Provider(ProviderName name, Address address, Email email, NumberPhone numberPhone)
         {
             Name = name;
             Address = address;
