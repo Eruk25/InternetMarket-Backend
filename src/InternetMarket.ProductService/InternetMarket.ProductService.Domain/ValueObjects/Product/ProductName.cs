6deb79bc7248cf1655347bf272b5769ce562ca5b
Danila <danilamatush72@gmail.com>
2026-04-02 12:24:31 +0300

    refactor(domain): move Description, Price, ProductName, Quantity to Product dir

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/ValueObjects/Product/ProductName.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.ProductService.Domain.ValueObjects
+{
+    public class ProductName
+    {
+        public string Value { get; }
+
+        private ProductName(string value) => Value = value;
+
+        public static ProductName Create(string value)
+        {
+            if (string.IsNullOrWhiteSpace(value))
+                throw new ArgumentNullException("Product name cannot be empty");
+            return new ProductName(value);
+        }
+    }
+}
