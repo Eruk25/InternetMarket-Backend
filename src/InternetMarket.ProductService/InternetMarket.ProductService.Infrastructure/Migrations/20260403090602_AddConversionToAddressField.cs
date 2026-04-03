2e98d5afdfb9b249db050a444bbfddf608f7666d
Danila <danilamatush72@gmail.com>
2026-04-03 12:08:09 +0300

    feat(infrastructure): add AddConversionToAddressField migration

---
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Migrations/20260403090602_AddConversionToAddressField.cs
@@ -0,0 +1,63 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.ProductService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddConversionToAddressField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address",
+                table: "Providers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_City",
+                table: "Providers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_Street",
+                table: "Providers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address_ZipCode",
+                table: "Providers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Address_City",
+                table: "Providers");
+
+            migrationBuilder.DropColumn(
+                name: "Address_Street",
+                table: "Providers");
+
+            migrationBuilder.DropColumn(
+                name: "Address_ZipCode",
+                table: "Providers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Address",
+                table: "Providers",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
