293292b40448ba11b88aef0b9d73df39b41e86a3
Danila <danilamatush72@gmail.com>
2026-04-03 11:59:10 +0300

    refactor(infrastructure): add Conversion to Address field

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/ProviderConfiguration.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/ProviderConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InternetMarket.ProductService.Domain.Entities;
+using InternetMarket.ProductService.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -14,13 +15,25 @@ namespace InternetMarket.ProductService.Infrastructure.Persistance.DB.Configurat
         {
             builder.HasKey(p => p.Id);
             builder.Property(p => p.Name)
-                .HasMaxLength(50);
-            builder.Property(p => p.Address)
-                .HasMaxLength(80);
+                .HasConversion(
+                    providerName => providerName.Value,
+                    value => ProviderName.Create(value))
+                .HasMaxLength(50)
+                .IsRequired();
+            builder.ComplexProperty(p => p.Address)
+                .IsRequired();
             builder.Property(p => p.Email)
-                .HasMaxLength(100);
+                .HasConversion(
+                    email => email.Value,
+                    value => Email.Create(value))
+                .HasMaxLength(100)
+                .IsRequired();
             builder.Property(p => p.NumberPhone)
-                .HasMaxLength(50);
+                .HasConversion(
+                    numberPhone => numberPhone.Value,
+                    value => NumberPhone.Create(value))
+                .HasMaxLength(50)
+                .IsRequired();
         }
     }
 }
