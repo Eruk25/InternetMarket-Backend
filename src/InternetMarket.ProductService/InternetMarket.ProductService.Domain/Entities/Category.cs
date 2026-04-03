a749e86e37c914f07f2e5011427caaba169f0699
Danila <danilamatush72@gmail.com>
2026-04-03 12:21:28 +0300

    refactor(domain): move to Provider dir

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Category.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/Entities/Category.cs
@@ -2,15 +2,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.ProductService.Domain.ValueObjects.Category;
 
 namespace InternetMarket.ProductService.Domain.Entities
 {
     public class Category
     {
         public Guid Id { get; private set; }
-        public string CategoryName { get; private set; }
+        public CategoryName CategoryName { get; private set; }
 
-        public Category(string categoryName)
+        public Category(CategoryName categoryName)
         {
             CategoryName = categoryName;
         }
