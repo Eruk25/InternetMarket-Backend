b55fb57c2bd21aa9987073c72b8fbced46472d52
Danila <danilamatush72@gmail.com>
2026-04-07 11:55:39 +0300

    refactor(application): remove MassTransit registration

--- src/InternetMarket.CartService/InternetMarket.CartService.Application/Extensions/DependencyInjection.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Application/Extensions/DependencyInjection.cs
@@ -1,9 +1,3 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using InternetMarket.CartService.Application.Consumers;
-using MassTransit;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace InternetMarket.CartService.Application.Extensions
@@ -13,19 +7,6 @@ namespace InternetMarket.CartService.Application.Extensions
         public static IServiceCollection AddApplication(this IServiceCollection services)
         {
             services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(DependencyInjection).Assembly));
-            services.AddMassTransit(x =>
-            {
-                x.AddConsumer<UserRegisteredConsumer>();
-                x.UsingRabbitMq((context, cfg) =>
-                {
-                    cfg.Host("localhost", "/", h =>
-                    {
-                        h.Username("guest");
-                        h.Password("guest");
-                    });
-                    cfg.ConfigureEndpoints(context);
-                });
-            });
             return services;
         }
     }
