66f24dca9db98c1d64a069a18850023c9fa5945f
Danila <danilamatush72@gmail.com>
2026-04-07 11:57:18 +0300

    feat(infrastructure): register MassTransit in DI

--- src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Extensions/DependencyInjection.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Extensions/DependencyInjection.cs
@@ -5,8 +5,10 @@ using System.Reflection.PortableExecutable;
 using System.Threading.Tasks;
 using InternetMarket.CartService.Application.Abstractions.Clients;
 using InternetMarket.CartService.Application.Abstractions.Repositories;
+using InternetMarket.CartService.Application.Consumers;
 using InternetMarket.CartService.Infrastructure.Implementations.Clients;
 using InternetMarket.CartService.Infrastructure.Implementations.Repositories;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -27,6 +29,28 @@ namespace InternetMarket.CartService.Infrastructure.Extensions
                 var productSection = configuration.GetSection("ProductService");
                 client.BaseAddress = new Uri(productSection["BaseUrl"]!);
             });
+
+            services.AddMassTransit(x =>
+            {
+                x.AddConsumer<UserRegisteredConsumer>();
+
+                x.AddEntityFrameworkOutbox<CartContext>(o =>
+                {
+                    o.UseSqlServer();
+                });
+
+                x.UsingRabbitMq((context, cfg) =>
+                {
+
+                    cfg.Host("localhost", "/", h =>
+                    {
+                        h.Username("guest");
+                        h.Password("guest");
+                    });
+                    cfg.ConfigureEndpoints(context);
+                });
+            });
+
             services.AddScoped<ICartRepository, CartRepository>();
             return services;
         }
