f4e094856fe0f95cf686925464ae69d0f15c48c3
Danila <danilamatush72@gmail.com>
2026-04-09 10:19:46 +0300

    refactor(infrastructure): add ChangeNameOfFieldFromTitleToProductName migration

---
+++ src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Migrations/20260409063427_ChangeNameOfFieldFromTitleToProductName.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.CartService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNameOfFieldFromTitleToProductName : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "CartItems",
+                newName: "ProductName");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "CartItems",
+                newName: "Title");
+        }
+    }
+}
