b6cb8044587a842275d116ccb34257e0a911bf1b
Danila <danilamatush72@gmail.com>
2026-04-09 10:27:47 +0300

    refactor(domain): change name of method from Title to ProductName

--- src/InternetMarket.CartService/InternetMarket.CartService.Domain/Entities/CartItem.cs
+++ src/InternetMarket.CartService/InternetMarket.CartService.Domain/Entities/CartItem.cs
@@ -13,14 +13,14 @@ namespace InternetMarket.CartService.Domain.Entities
         public Guid CartId { get; set; }
         public Cart? Cart { get; set; }
         public Guid ProductId { get; set; }
-        public string Title { get; set; }
+        public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Quantity { get; set; }
 
-        public CartItem(Guid productId, string title, decimal price, int quantity)
+        public CartItem(Guid productId, string productName, decimal price, int quantity)
         {
             ProductId = productId;
-            Title = title;
+            ProductName = productName;
             Price = price;
             Quantity = quantity;
         }
