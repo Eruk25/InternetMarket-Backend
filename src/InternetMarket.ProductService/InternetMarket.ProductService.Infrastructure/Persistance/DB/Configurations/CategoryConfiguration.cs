e37922607a03f9a6df358381b98e7511c0087506
Danila <danilamatush72@gmail.com>
2026-04-09 10:24:37 +0300

    feat(infrastructure): add Conversion for CategoryName

--- src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/CategoryConfiguration.cs
+++ src/InternetMarket.ProductService/InternetMarket.ProductService.Infrastructure/Persistance/DB/Configurations/CategoryConfiguration.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using InternetMarket.ProductService.Domain.Entities;
+using InternetMarket.ProductService.Domain.ValueObjects.Category;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
@@ -14,6 +15,9 @@ namespace InternetMarket.ProductService.Infrastructure.Persistance.DB.Configurat
         {
             builder.HasKey(c => c.Id);
             builder.Property(c => c.CategoryName)
+                .HasConversion(
+                    categoryName => categoryName.Value,
+                    value => CategoryName.Create(value))
                 .HasMaxLength(80);
         }
     }
