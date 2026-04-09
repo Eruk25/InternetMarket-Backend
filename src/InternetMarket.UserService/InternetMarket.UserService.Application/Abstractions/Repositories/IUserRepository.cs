463574a2b8e9e81d844e4365d111eb895b79de65
Danila <danilamatush72@gmail.com>
2026-04-09 10:21:27 +0300

    refactor(application): change parameter from string to Email

--- src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IUserRepository.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Application/Abstractions/Repositories/IUserRepository.cs
@@ -1,4 +1,5 @@
 using InternetMarket.UserService.Domain.Entities;
+using InternetMarket.UserService.Domain.ValueObjects;
 
 namespace InternetMarket.UserService.Application.Abstractions.Repositories
 {
@@ -6,7 +7,7 @@ namespace InternetMarket.UserService.Application.Abstractions.Repositories
     {
         public Task<IEnumerable<User>> GetAllAsync();
         public Task<User?> GetByIdAsync(Guid Id);
-        public Task<User?> GetByEmailAsync(string Email);
+        public Task<User?> GetByEmailAsync(Email email);
         public Task CreateAsync(User user);
         public Task UpdateAsync(User user);
         public Task DeleteAsync(User user);
