8ddcd67ac7600455cf69ad9854a8892825cb25c8
Danila <danilamatush72@gmail.com>
2026-04-15 17:59:46 +0300

    fix(domain): change type of field to User?

--- src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/EmailVerificationToken.cs
+++ src/InternetMarket.UserService/InternetMarket.UserService.Domain/Entities/EmailVerificationToken.cs
@@ -9,7 +9,7 @@ namespace InternetMarket.UserService.Domain.Entities
     {
         public Guid Id { get; set; }
         public Guid UserId { get; set; }
-        public User User { get; set; }
+        public User? User { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime ExpiresAt { get; set; }
     }
