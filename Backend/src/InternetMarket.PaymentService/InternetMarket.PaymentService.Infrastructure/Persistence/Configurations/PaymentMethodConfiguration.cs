25b4ac13b559e6e583dfa4f2daa45282b07dbc64
Danila <danilamatush72@gmail.com>
2026-04-19 10:19:18 +0300

    feat(infrastructure): add SeedData method

--- Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Persistence/Configurations/PaymentMethodConfiguration.cs
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Persistence/Configurations/PaymentMethodConfiguration.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using InternetMarket.PaymentService.Domain.Entities;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Diagnostics;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace InternetMarket.PaymentService.Infrastructure.Persistence.Configurations
@@ -21,6 +22,25 @@ namespace InternetMarket.PaymentService.Infrastructure.Persistence.Configuration
                 .IsRequired();
             builder.Property(pm => pm.IsActive)
                 .IsRequired();
+            SeedData(builder);
+        }
+
+        public static void SeedData(EntityTypeBuilder<PaymentMethod> builder)
+        {
+            builder.HasData(new
+            {
+                Id = Guid.Parse("4dea45ad-4dbf-4ae2-b589-cb442554e357"),
+                Name = "Банковская карта",
+                SystemName = "Card",
+                IsActive = true
+            },
+            new
+            {
+                Id = Guid.Parse("f40f776b-49cf-4d0e-b209-bb7a62ca6eb9"),
+                Name = "Наличные",
+                SystemName = "Cash",
+                IsActive = true
+            });
         }
     }
 }
