e3511659d78fb67fcc1db7f7b162aa2628e6e669
Danila <danilamatush72@gmail.com>
2026-04-24 15:49:14 +0300

    feat(infrastructure): register Indbox and OutBox relations

--- Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Persistence/DB/PaymentContext.cs
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Persistence/DB/PaymentContext.cs
@@ -4,21 +4,24 @@ using System.Linq;
 using System.Threading.Tasks;
 using InternetMarket.PaymentService.Domain.Entities;
 using InternetMarket.PaymentService.Infrastructure.Persistence.Configurations;
+using MassTransit;
 using Microsoft.EntityFrameworkCore;
 
 namespace InternetMarket.PaymentService.Infrastructure.Persistence.DB
 {
     public class PaymentContext(DbContextOptions<PaymentContext> options) : DbContext(options)
     {
-        DbSet<Transaction> Transactions { get; set; }
-        DbSet<PaymentMethod> PaymentMethods { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<PaymentMethod> PaymentMethods { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-            base.OnModelCreating(modelBuilder);
-
             modelBuilder.ApplyConfiguration(new PaymentMethodConfiguration());
             modelBuilder.ApplyConfiguration(new TransactionConfiguration());
+
+            modelBuilder.AddInboxStateEntity();
+            modelBuilder.AddOutboxMessageEntity();
+            modelBuilder.AddOutboxStateEntity();
         }
     }
 }
