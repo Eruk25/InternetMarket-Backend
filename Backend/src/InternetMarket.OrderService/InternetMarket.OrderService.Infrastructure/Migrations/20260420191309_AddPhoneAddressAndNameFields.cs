12f5b767f9f9cc0a7427ff0f431626218d535b83
Danila <danilamatush72@gmail.com>
2026-04-24 15:26:03 +0300

    feat(infrastructure): add AddPhoneAddressAndNameFields migration

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260420191309_AddPhoneAddressAndNameFields.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddPhoneAddressAndNameFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Orders",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "CustomerPhone",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "FirstName",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "LastName",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Orders",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CustomerPhone",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "FirstName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "LastName",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Orders");
+        }
+    }
+}
