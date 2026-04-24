13cc650be18521d9fd6b83ca5a5d749ffcc266ba
Danila <danilamatush72@gmail.com>
2026-04-24 15:53:26 +0300

    feat(infrastructure): add AddTokenOrderIdCreatedAtAndUpdatedAtFields migration

---
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Migrations/20260420192642_AddTokenOrderIdCreatedAtAndUpdatedAtFields.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.PaymentService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTokenOrderIdCreatedAtAndUpdatedAtFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CreatedAt",
+                table: "Transactions",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExternalToken",
+                table: "Transactions",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "OrderId",
+                table: "Transactions",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"));
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreatedAt",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "ExternalToken",
+                table: "Transactions");
+
+            migrationBuilder.DropColumn(
+                name: "OrderId",
+                table: "Transactions");
+        }
+    }
+}
