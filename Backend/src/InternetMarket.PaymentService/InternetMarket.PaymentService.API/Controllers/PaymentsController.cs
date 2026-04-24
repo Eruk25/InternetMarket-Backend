c5f560e6cd03798f1c54c91e9a2ad45affcc65f4
Danila <danilamatush72@gmail.com>
2026-04-24 15:26:45 +0300

    feat(presentation): add Pay and ProcessWebgook methods

---
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.API/Controllers/PaymentsController.cs
@@ -0,0 +1,46 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using System.Transactions;
+using InternetMarket.PaymentService.Application.Transactions.Create;
+using InternetMarket.PaymentService.Application.Transactions.Update;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace InternetMarket.PaymentService.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PaymentsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public PaymentsController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost]
+        [Route("pay/{orderId}")]
+        public async Task<IActionResult> PayAsync([FromRoute] Guid orderId)
+        {
+            var link = await _mediator.Send(new CreateTransactionCommand(orderId));
+            return Ok(link);
+        }
+
+        [HttpPost]
+        [Route("webhook")]
+        public async Task<IActionResult> ProcessWebhookAsync()
+        {
+            using var reader = new StreamReader(Request.Body);
+            var json = await reader.ReadToEndAsync();
+
+            await _mediator.Send(new UpdateTransactionStatusCommand(json));
+
+            return Ok();
+        }
+    }
+}
