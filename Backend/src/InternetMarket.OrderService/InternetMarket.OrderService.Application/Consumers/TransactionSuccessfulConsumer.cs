f087cba7ea439fc26f2b07e6270b367e8c6882eb
Danila <danilamatush72@gmail.com>
2026-04-24 15:11:01 +0300

    feat(application): add TransactionSuccessfulConsumer

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Application/Consumers/TransactionSuccessfulConsumer.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Formats.Asn1;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Transactions;
+using InternetMarket.Contracts.Events.Transaction;
+using InternetMarket.OrderService.Application.Orders.Update.UpdateStatus;
+using MassTransit;
+using MediatR;
+
+namespace InternetMarket.OrderService.Application.Consumers
+{
+    public class TransactionSuccessfulConsumer : IConsumer<TransactionSuccessful>
+    {
+        private readonly ISender _sender;
+
+        public TransactionSuccessfulConsumer(ISender sender)
+        {
+            _sender = sender;
+        }
+
+        public async Task Consume(ConsumeContext<TransactionSuccessful> context)
+        {
+            await _sender.Send(new UpdateOrderStatusCommand(context.Message.OrderId));
+        }
+    }
+}
