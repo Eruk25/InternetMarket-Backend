ba49ddc8e2b55797cd07d96670e361ba41921b13
Danila <danilamatush72@gmail.com>
2026-04-24 15:38:18 +0300

    feat(application): add UpdateTransactionStatusCommandHandler

---
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Application/Transactions/Update/UpdateTransactionStatusCommandHandler.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json;
+using System.Threading.Tasks;
+using InternetMarket.Contracts.Events.Transaction;
+using InternetMarket.PaymentService.Application.Abstractions.UnitOfWork;
+using InternetMarket.PaymentService.Application.DTOs.PaymentGateway.Response;
+using InternetMarket.PaymentService.Domain.ValueObjects;
+using MassTransit;
+using MediatR;
+
+namespace InternetMarket.PaymentService.Application.Transactions.Update
+{
+    public class UpdateTransactionStatusCommandHandler : IRequestHandler<UpdateTransactionStatusCommand>
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IPublishEndpoint _publishEndpoint;
+
+        public UpdateTransactionStatusCommandHandler(IUnitOfWork unitOfWork, IPublishEndpoint publishEndpoint)
+        {
+            _unitOfWork = unitOfWork;
+            _publishEndpoint = publishEndpoint;
+        }
+
+        public async Task Handle(UpdateTransactionStatusCommand request, CancellationToken cancellationToken)
+        {
+            var notification = JsonSerializer.Deserialize<BePaidWebhookDto>(request.Json);
+
+            if (notification is null)
+                throw new ArgumentNullException("BePaid notification is null");
+
+            var transaction = await _unitOfWork.Transactions.GetByOrderIdAsync(Guid.Parse(notification.Transaction.TrakingId));
+
+            if (transaction is null)
+                throw new ArgumentNullException("Transaction was not found");
+            transaction.ConfirmSuccess();
+
+            await _publishEndpoint.Publish(new TransactionSuccessful(transaction.OrderId));
+            await _unitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
