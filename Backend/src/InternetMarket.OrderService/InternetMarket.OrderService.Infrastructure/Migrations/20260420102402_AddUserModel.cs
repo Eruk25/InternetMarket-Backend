0932b6fda5328386c4f5a1a42d6749f174167286
Danila <danilamatush72@gmail.com>
2026-04-24 15:25:23 +0300

    feat(infrastructure): add AddUserModel migration

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260420102402_AddUserModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Title",
+                table: "OrderItems",
+                newName: "ProductName");
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "ProductName",
+                table: "OrderItems",
+                newName: "Title");
+        }
+    }
+}
