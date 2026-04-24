39553f7076be6665ceff0a0e2e74808f31aa77c6
Danila <danilamatush72@gmail.com>
2026-04-24 15:52:59 +0300

    feat(infrastructure): add ChnageNameOfFieldToPaymentDate migration

---
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Infrastructure/Migrations/20260424072135_ChangeNameOfFieldToPaymentDate.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.PaymentService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class ChangeNameOfFieldToPaymentDate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UpdatedAt",
+                table: "Transactions",
+                newName: "PaymentDate");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "PaymentDate",
+                table: "Transactions",
+                newName: "UpdatedAt");
+        }
+    }
+}
