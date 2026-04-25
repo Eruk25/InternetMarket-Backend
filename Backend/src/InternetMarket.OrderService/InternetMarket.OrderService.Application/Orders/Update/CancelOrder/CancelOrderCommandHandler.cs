217a84e35c306cebaacfc0599929fdd90078d628
Danila <danilamatush72@gmail.com>
2026-04-25 19:02:15 +0300

    refactor(application): use IUserRepository

--- Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Application/Orders/Update/CancelOrder/CancelOrderCommandHandler.cs
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Application/Orders/Update/CancelOrder/CancelOrderCommandHandler.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using InternetMarket.Contracts.Events.Order;
 using InternetMarket.OrderService.Application.Abstractions.Repositories;
 using InternetMarket.OrderService.Application.Abstractions.UnitOfWork;
+using InternetMarket.OrderService.Domain.Entities;
 using MassTransit;
 using MediatR;
 
@@ -13,12 +14,15 @@ namespace InternetMarket.OrderService.Application.Orders.Delete
     public class CancelOrderCommandHandler : IRequestHandler<CancelOrderCommand>
     {
         private readonly IOrderRepository _orderRepository;
+        private readonly IUserRepository _userRepository;
         private readonly IPublishEndpoint _publishEndpoint;
         private readonly IUnitOfWork _unitOfWork;
 
-        public CancelOrderCommandHandler(IOrderRepository orderRepository, IPublishEndpoint publishEndpoint, IUnitOfWork unitOfWork)
+        public CancelOrderCommandHandler(IOrderRepository orderRepository, IPublishEndpoint publishEndpoint, IUnitOfWork unitOfWork,
+         IUserRepository userRepository)
         {
             _orderRepository = orderRepository;
+            _userRepository = userRepository;
             _publishEndpoint = publishEndpoint;
             _unitOfWork = unitOfWork;
         }
@@ -30,12 +34,17 @@ namespace InternetMarket.OrderService.Application.Orders.Delete
             if (order is null)
                 throw new ArgumentNullException("Order was not found");
 
+            var user = await _userRepository.GetByIdAsync(order.UserId);
+
+            if (user is null)
+                throw new ArgumentNullException("User was not found");
+
             order.Cancel();
             await _orderRepository.UpdateAsync(order);
 
             await _publishEndpoint.Publish(new OrderCancelled(
                 order.Id,
-                request.Email
+                user.Email.Value
             ));
 
             await _unitOfWork.SaveChangesAsync(cancellationToken);
