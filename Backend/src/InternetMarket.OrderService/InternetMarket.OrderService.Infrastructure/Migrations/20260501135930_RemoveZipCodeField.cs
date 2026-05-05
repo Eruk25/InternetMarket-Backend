07cefb01fe013f316a9d9c0ced001c51f4f7db11
Danila <danilamatush72@gmail.com>
2026-05-05 14:38:15 +0300

    feat(infrastructure): add RemoveZipCodeField migration

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260501135930_RemoveZipCodeField.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveZipCodeField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ZipCode",
+                table: "Orders");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ZipCode",
+                table: "Orders",
+                type: "nvarchar(6)",
+                maxLength: 6,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
