6f91f89fd450eb547bf8cfb3655ae9a2bfa7e593
Danila <danilamatush72@gmail.com>
2026-05-07 16:07:30 +0300

    feat(domain): add Add and Subtract methods

--- Backend/src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/ValueObjects/Product/Quantity.cs
+++ Backend/src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/ValueObjects/Product/Quantity.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.Marshalling;
 using System.Threading.Tasks;
 
 namespace InternetMarket.ProductService.Domain.ValueObjects
@@ -13,9 +14,12 @@ namespace InternetMarket.ProductService.Domain.ValueObjects
 
         public static Quantity Create(int value)
         {
-            if (value <= 0 || int.MaxValue < value)
+            if (value < 0)
                 throw new ArgumentException($"Quantity cannot be equal or less then 0 or more {int.MaxValue}");
             return new Quantity(value);
         }
+
+        public Quantity Add(int value) => Create(this.Value + value);
+        public Quantity Subtract(int value) => Create(this.Value - value);
     }
 }
