4258f335a818d79592383d18b59339bb0a800c8a
Danila <danilamatush72@gmail.com>
2026-05-21 21:47:18 +0300

    feat(infrastructure): add DependencyInjection method

---
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Infrastructure/Extensions/DependencyInjection.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ShipmentService.Application.Abstractions.Clients;
+using InternetMarket.ShipmentService.Application.Abstractions.Repositories;
+using InternetMarket.ShipmentService.Application.Abstractions.UnitOfWork;
+using InternetMarket.ShipmentService.Infrastructure.Implementations.Clients;
+using InternetMarket.ShipmentService.Infrastructure.Implementations.Repositories;
+using InternetMarket.ShipmentService.Infrastructure.Implementations.UnitOfWork;
+using InternetMarket.ShipmentService.Infrastructure.Persistence.DB;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace InternetMarket.ShipmentService.Infrastructure.Extensions
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionSection = configuration.GetSection("ConnectionStrings");
+            var connectionString = connectionSection["DefaultConnection"];
+            services.AddDbContext<ShipmentContext>(option =>
+                option.UseSqlServer(connectionString));
+
+            services.Configure<CdekOptions>(configuration.GetSection("ShipmentService"));
+            services.AddScoped<IUnitOfWork, UnitOfWork>();
+            services.AddScoped<IShipmentRepository, ShipmentRepository>();
+            services.AddHttpClient<IShipmentClient, ShipmentClient>(client =>
+            {
+                var shipmentSection = configuration.GetSection("ShipmentService");
+                client.BaseAddress = new Uri(shipmentSection["BaseUrl"]!);
+            });
+            services.AddStackExchangeRedisCache(options =>
+            {
+                options.Configuration = "127.0.0.1:6379";
+                options.InstanceName = "local";
+            });
+            return services;
+        }
+    }
+}
