5115a953a313dc9c681fc37214b50612d2edab5b
Danila <danilamatush72@gmail.com>
2026-05-23 21:45:16 +0300

    feat(domain): add Length vo

---
+++ Backend/src/InternetMarket.ProductService/InternetMarket.ProductService.Domain/ValueObjects/Product/Length.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.ProductService.Domain.ValueObjects.Product
+{
+    public record Length
+    {
+        public int Value { get; }
+
+        private Length(int value) { Value = value; }
+
+        public static Length Create(int value)
+        {
+            if (value <= 0)
+                throw new ArgumentException("Lenght can not be equal or less then 0");
+
+            return new Length(value);
+        }
+    }
+}
