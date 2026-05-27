e926fd56f2bb24e4d42f507e51bba58e6a0b209b
Danila <danilamatush72@gmail.com>
2026-05-27 11:57:23 +0300

    feat(application): add CreateShipmentCommand

---
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Application/Shipments/Create/CreateShipmentCommand.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.ShipmentService.Application.DTOs;
+using MediatR;
+
+namespace InternetMarket.ShipmentService.Application.Shipments.Create
+{
+    public record CreateShipmentCommand(int DeliveryType, int ToCityCode, string? DeliveryPointId, string City, string Address,
+     string FullName, string NumberPhone, Guid OrderId, IEnumerable<OrderItemDto> OrderItems) : IRequest<CreateOrderDeliveryResponse>;
+}
