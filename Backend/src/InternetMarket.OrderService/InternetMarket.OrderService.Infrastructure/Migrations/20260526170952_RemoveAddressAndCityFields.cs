b9a955d2a89e3bec46d7197cd33b73872315ee11
Danila <danilamatush72@gmail.com>
2026-05-27 11:38:34 +0300

    feat(infrastructure): add RemoveAddressAndCityFields migration

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Migrations/20260526170952_RemoveAddressAndCityFields.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.OrderService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveAddressAndCityFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "Street",
+                table: "Orders");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLargeSizeProduct",
+                table: "OrderItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Length",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Weight",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "OrderItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsLargeSizeProduct",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "OrderItems");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "OrderItems");
+
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Orders",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Street",
+                table: "Orders",
+                type: "nvarchar(50)",
+                maxLength: 50,
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
