4e8c1d8f3215e424fac342121b6cb608c8bce2a4
Danila <danilamatush72@gmail.com>
2026-05-27 12:56:10 +0300

    feat(infrastructure): add PackageItem

---
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Infrastructure/Implementations/Clients/DTOs/Requests/PackageItem.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+using Microsoft.Identity.Client;
+
+namespace InternetMarket.ShipmentService.Infrastructure.Implementations.Clients.DTOs.Requests
+{
+    public class PackageItem
+    {
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("ware_key")]
+        public string WareKey { get; set; }
+        [JsonPropertyName("payment")]
+        public Payment Payment { get; set; }
+        [JsonPropertyName("weight")]
+        public int Weight { get; set; }
+        [JsonPropertyName("amount")]
+        public int Amount { get; set; }
+        [JsonPropertyName("cost")]
+        public decimal Cost { get; set; }
+
+        public PackageItem(string name, string wareKey, int weight, int amount, decimal cost)
+        {
+            Name = name;
+            WareKey = wareKey;
+            Payment = new Payment(0);
+            Weight = weight;
+            Amount = amount;
+            Cost = cost;
+        }
+    }
+}
