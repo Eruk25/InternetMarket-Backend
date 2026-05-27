f96faa2f97dee31a1888284d1d2574ce27c9887f
Danila <danilamatush72@gmail.com>
2026-05-27 11:55:02 +0300

    refactor(application): add OrderItems field and rename TypeOfDelivery to DeliveryType

--- Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Application/Shipments/Calculate/CalculateDeliveryPriceCommand.cs
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Application/Shipments/Calculate/CalculateDeliveryPriceCommand.cs
@@ -2,9 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.ShipmentService.Application.DTOs;
 using MediatR;
 
 namespace InternetMarket.ShipmentService.Application.Shipments.Calculate
 {
-    public record CalculateDeliveryPriceCommand(int ToCityCode, int TypeOfDelivery) : IRequest<CalculateDeliveryPriceResponse?>;
+    public record CalculateDeliveryPriceCommand(int DeliveryType, int ToCityCode, IEnumerable<OrderItemDto> OrderItems) : IRequest<CalculateDeliveryPriceResponse?>;
 }
