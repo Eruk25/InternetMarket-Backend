13d5428df4d43404b631341f8ed6574e554addbc
Danila <danilamatush72@gmail.com>
2026-05-27 11:48:20 +0300

    refactor(application): remove GetOrderInfo method

--- Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Application/Abstractions/Clients/IShipmentClient.cs
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.Application/Abstractions/Clients/IShipmentClient.cs
@@ -2,9 +2,14 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
 using InternetMarket.ShipmentService.Application.DTOs;
 using InternetMarket.ShipmentService.Application.Shipments.Calculate;
+using InternetMarket.ShipmentService.Application.Shipments.Create;
+using InternetMarket.ShipmentService.Application.Shipments.Get;
+using InternetMarket.ShipmentService.Application.Shipments.Get.GetDeliveryPoints;
+using InternetMarket.ShipmentService.Domain.ValueObjects;
 
 namespace InternetMarket.ShipmentService.Application.Abstractions.Clients
 {
@@ -13,8 +18,8 @@ namespace InternetMarket.ShipmentService.Application.Abstractions.Clients
         Task<string> GetTokenAsync();
         Task<string> RefreshTokenAsync();
         Task<IEnumerable<ShipmentCityResponse>?> GetCitiesAsync(string name, CancellationToken cancellationToken = default);
-        Task CreateOrderAsync();
-        Task<object> GetOrderInfo(Guid id);
-        Task<CalculateDeliveryPriceResponse?> CalculateTariffAsync(int toCityCode, int typeOfDelivery, CancellationToken cancellationToken = default);
+        Task<IEnumerable<DeliveryPointResponse>?> GetDeliveryPointsAsync(int cityCode, CancellationToken cancellationToken = default);
+        Task<CreateOrderDeliveryResponse> CreateOrderAsync(int? toCityCode, string? deliveryPointId, DeliveryType deliveryType, string? City, string? address, string fullName, string numberPhone, IEnumerable<OrderItemDto> orderItems, CancellationToken cancellationToken = default);
+        Task<CalculateDeliveryPriceResponse?> CalculateTariffAsync(int toCityCode, DeliveryType deliveryType, IEnumerable<OrderItemDto> orderItems, CancellationToken cancellationToken = default);
     }
 }
