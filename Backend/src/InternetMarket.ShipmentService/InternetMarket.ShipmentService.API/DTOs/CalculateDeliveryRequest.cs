52a14b49d02d9d817a0a4bf47fd3d277fd3b4566
Danila <danilamatush72@gmail.com>
2026-05-27 11:43:05 +0300

    feat(presentation): add DeliveryPointId and OrderItems fields

--- Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.API/DTOs/CalculateDeliveryRequest.cs
+++ Backend/src/InternetMarket.ShipmentService/InternetMarket.ShipmentService.API/DTOs/CalculateDeliveryRequest.cs
@@ -1,9 +1,17 @@
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using InternetMarket.ShipmentService.API.DTOs.Order;
 
 namespace InternetMarket.ShipmentService.API.DTOs
 {
-    public record CalculateDeliveryRequest(int ToCityCode, int TypeOfDelivery);
+    public record CalculateDeliveryRequest
+    {
+        public int DeliveryType { get; set; }
+        public int ToCityCode { get; set; }
+        public string? DeliveryPointId { get; set; }
+        public IEnumerable<OrderItemDto> OrderItems { get; set; } = default!;
+    };
 }
