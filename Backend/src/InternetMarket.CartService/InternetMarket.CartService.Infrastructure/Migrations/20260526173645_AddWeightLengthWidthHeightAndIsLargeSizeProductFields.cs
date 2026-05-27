3d5bfb85523267419d4b90bba481a8e684422da9
Danila <danilamatush72@gmail.com>
2026-05-27 11:17:32 +0300

    feat(infrastructure): add AddWeightLengthWidthHeightAndIsLargeSizeProductFields migration

---
+++ Backend/src/InternetMarket.CartService/InternetMarket.CartService.Infrastructure/Migrations/20260526173645_AddWeightLengthWidthHeightAndIsLargeSizeProductFields.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InternetMarket.CartService.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddWeightLengthWidthHeightAndIsLargeSizeProductFields : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Height",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<bool>(
+                name: "IsLargeSizeProduct",
+                table: "CartItems",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Length",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Weight",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Width",
+                table: "CartItems",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Height",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "IsLargeSizeProduct",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Length",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Weight",
+                table: "CartItems");
+
+            migrationBuilder.DropColumn(
+                name: "Width",
+                table: "CartItems");
+        }
+    }
+}
