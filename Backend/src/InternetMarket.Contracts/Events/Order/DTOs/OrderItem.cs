708928d22480dd3c820aed76d899510dfb09a270
Danila <danilamatush72@gmail.com>
2026-05-27 11:18:39 +0300

    feat: add weight, length, width, height, isLargeSizeProduct fields

--- Backend/src/InternetMarket.Contracts/Events/Order/DTOs/OrderItem.cs
+++ Backend/src/InternetMarket.Contracts/Events/Order/DTOs/OrderItem.cs
@@ -5,5 +5,6 @@ using System.Threading.Tasks;
 
 namespace InternetMarket.Contracts.Events.Order.DTOs
 {
-    public record OrderItem(string Title, int Quantity, decimal UnitPrice);
+    public record OrderItem(Guid ProductId, string ProductName, int Quantity, decimal UnitPrice, int Weight,
+     int Length, int Width, int Height, bool IsLargeSizeProduct);
 }
---
+++ Backend/src/InternetMarket.Contracts/Events/Shipment/ShipmentCreated.cs
@@ -0,0 +1,9 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace InternetMarket.Contracts.Events.Shipment
+{
+    public record ShipmentCreated(Guid OrderId);
+}
