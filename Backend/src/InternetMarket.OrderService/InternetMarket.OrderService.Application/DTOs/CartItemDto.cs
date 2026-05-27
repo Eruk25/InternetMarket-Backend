7d84a3206837ebae5acfb676c17ce2baee11f113
Danila <danilamatush72@gmail.com>
2026-05-27 11:28:15 +0300

    feat(application): add Weight, Length, Width, Height, IsLargeSizeProduct fields

--- Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Application/DTOs/CartItemDto.cs
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Application/DTOs/CartItemDto.cs
@@ -5,5 +5,5 @@ using System.Threading.Tasks;
 
 namespace InternetMarket.OrderService.Application.DTOs
 {
-    public record CartItemDto(Guid ProductId, string Title, decimal Price, int Quantity);
+    public record CartItemDto(Guid ProductId, string Title, decimal Price, int Quantity, int Weight, int Length, int Width, int Height, bool IsLargeSizeProduct);
 }
