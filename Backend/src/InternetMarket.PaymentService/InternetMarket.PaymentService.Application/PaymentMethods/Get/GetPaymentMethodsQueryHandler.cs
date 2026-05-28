ac731ccc67ddb74b3853ee05c34235ebb711741e
Danila <danilamatush72@gmail.com>
2026-05-28 12:17:49 +0300

    feat(application): implement GetPaymentMethodsQuery in GetPaymentMethodsQueryHandler

---
+++ Backend/src/InternetMarket.PaymentService/InternetMarket.PaymentService.Application/PaymentMethods/Get/GetPaymentMethodsQueryHandler.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using InternetMarket.PaymentService.Application.Abstractions.Repositories;
+using MediatR;
+
+namespace InternetMarket.PaymentService.Application.PaymentMethods.Get
+{
+    public class GetPaymentMethodsQueryHandler : IRequestHandler<GetPaymentMethodsQuery, IEnumerable<PaymentMethodDto>>
+    {
+        private readonly IPaymentMehtodRepository _paymentMehtodRepository;
+
+        public GetPaymentMethodsQueryHandler(IPaymentMehtodRepository paymentMehtodRepository)
+        {
+            _paymentMehtodRepository = paymentMehtodRepository;
+        }
+
+        public async Task<IEnumerable<PaymentMethodDto>> Handle(GetPaymentMethodsQuery request, CancellationToken cancellationToken)
+        {
+            var paymentMethods = await _paymentMehtodRepository.GetAllAsync(cancellationToken);
+
+            return paymentMethods.Select(pm => new PaymentMethodDto(pm.Name, pm.SystemName, pm.IsActive));
+        }
+    }
+}
