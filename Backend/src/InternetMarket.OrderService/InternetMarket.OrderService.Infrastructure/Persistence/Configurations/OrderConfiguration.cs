882442c7bee4fc66f82cc8c0f3cde6800551de1d
Danila <danilamatush72@gmail.com>
2026-05-29 19:07:50 +0300

    feat(infrastructure): add DeliveryInfo and PaymentMethod to configuration

--- Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Infrastructure/Persistence/Configurations/OrderConfiguration.cs
@@ -6,6 +6,7 @@ using InternetMarket.OrderService.Domain.Entities;
 using InternetMarket.OrderService.Domain.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.VisualBasic;
 
 namespace InternetMarket.OrderService.Infrastructure.Persistence.Configurations
 {
@@ -20,21 +21,40 @@ namespace InternetMarket.OrderService.Infrastructure.Persistence.Configurations
             {
                 navigatorBuilder.Property(n => n.FirstName)
                     .HasColumnName("FirstName")
-                    .IsRequired()
+                    .IsRequired(true)
                     .HasMaxLength(50);
                 navigatorBuilder.Property(n => n.LastName)
                     .HasColumnName("LastName")
-                    .IsRequired()
+                    .IsRequired(true)
                     .HasMaxLength(50);
+            });
+            builder.Property(o => o.PaymentMethod)
+                .HasConversion(
+                    paymentMethod => paymentMethod.Value,
+                    value => PaymentMethod.FromValue(value))
+                .IsRequired(true);
+            builder.OwnsOne(o => o.DeliveryInfo, navigationBulder =>
+            {
+                navigationBulder.Property(di => di.DeliveryType)
+                    .HasColumnName("DeliveryType")
+                    .IsRequired(true);
+                navigationBulder.Property(di => di.ToCityCode)
+                    .HasColumnName("ToCityCode");
+                navigationBulder.Property(di => di.DeliveryPointId)
+                    .HasColumnName("DeliveryPointId");
+                navigationBulder.Property(di => di.City)
+                    .HasColumnName("City");
+                navigationBulder.Property(di => di.Address)
+                    .HasColumnName("Address");
             });
             builder.Property(p => p.CustomerPhone)
                 .HasConversion(
                     numberPhone => numberPhone.Value,
                     value => NumberPhone.Create(value))
                 .HasMaxLength(50)
-                .IsRequired();
+                .IsRequired(true);
             builder.Property(o => o.TotalPrice)
-                .IsRequired();
+                .IsRequired(true);
             builder.Property(o => o.PaymentDate)
                 .IsRequired(false);
             builder.Property(o => o.Status)
@@ -42,9 +62,9 @@ namespace InternetMarket.OrderService.Infrastructure.Persistence.Configurations
                     status => status.Value,
                     value => OrderStatus.FromValue(value)
                 )
-                .IsRequired();
+                .IsRequired(true);
             builder.Property(o => o.CreatedAt)
-                .IsRequired();
+                .IsRequired(true);
             builder.Property(o => o.UpdatedAt);
             builder.HasMany(o => o.OrderItems)
                 .WithOne(oi => oi.Order)
