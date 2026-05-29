f616e304774a9753eec07b0f24e71c5b8511c30e
Danila <danilamatush72@gmail.com>
2026-05-29 19:06:14 +0300

    feat(domain): add PaymentMethod vo

---
+++ Backend/src/InternetMarket.OrderService/InternetMarket.OrderService.Domain/ValueObjects/PaymentMethod.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Ardalis.SmartEnum;
+
+namespace InternetMarket.OrderService.Domain.ValueObjects
+{
+    public class PaymentMethod : SmartEnum<PaymentMethod>
+    {
+        public static readonly PaymentMethod Cash = new PaymentMethod(nameof(Cash), 0);
+        public static readonly PaymentMethod Card = new PaymentMethod(nameof(Card), 1);
+
+        public PaymentMethod(string name, int value) : base(name, value) { }
+    }
+}
